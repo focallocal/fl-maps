94365f9f24350a96538e9e165569529583fa80cd
mordka <pawel.smile@gmail.com>
2014-11-02 14:49:58 +0000

    working todo-like app

--- events-app.js
+++ events-app.js
@@ -1,19 +1,38 @@
+Events = new Mongo.Collection("events");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+  // This code only runs on the client
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+  Template.body.helpers({
+    events: function () {
+      return Events.find({},{sort:{createdAt: -1}});
     }
+
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.body.events({
+
+    "submit .new-event": function (event) {
+      console.info(event)
+      var text = event.target.text.value;
+      Events.insert({
+        event_name: text,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
+
+      return false;
     }
-  });
+
+   });
+  Template.event.events({
+    "click .toggle-checked": function () {
+      Events.updaet(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Events.remove(this._id);
+    }
+  })
 }
 
 if (Meteor.isServer) {
