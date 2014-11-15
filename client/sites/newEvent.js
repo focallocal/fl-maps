8c717bd91e8c4688fbf4143449ded07b96d3340c
mordka <pawel.smile@gmail.com>
2014-11-15 14:20:45 +0000

    providing data to save works

--- client/sites/newEvent.js
+++ client/sites/newEvent.js
@@ -6,3 +6,33 @@ Template.newEvent.helpers({
      return Session.get("createCoords");
  }
 });
+Template.newEvent.events({
+   'click .special': function (event, template) {
+       var title = template.find("#title").value;
+       var location = template.find("#location").value;
+       var artifact = template.find("#artifact").value;
+       var category = template.find("#category").value;
+       var description = template.find("#description").value;
+       var latlng = Session.get("createCoords");
+
+       if (title.length && location.length &&
+           category.length && category.length &&
+           description.length) {
+           Meteor.call('createEvent', {
+               title: title,
+                location : location,
+                artifact : artifact,
+                category : category,
+                description : description,
+                latlng : latlng
+           }, function (error, event)  {
+              if (! error) {
+                  Session.set('selected', event);
+              }
+           });
+           $("#newEvent").modal("hide");
+       } else {
+           Session.set("newEventError", "Please fill all required details");
+       }
+   }
+});
--- main.js
+++ main.js
@@ -33,6 +33,42 @@ if (Meteor.isClient) {
         $mc = $('#map_canvas');
         $mc.css('height',(h-offsetTop));
     }).resize();
+
+    Meteor.methods({
+        createEvent: function (options) {
+            console.log('trying to save event');
+            if (! (typeof options.title === "string" && options.title.length &&
+                typeof options.location === "string" && options.location.length &&
+                typeof options.artifact === "string" && options.artifact.length &&
+                typeof options.category === "string" && options.category.length &&
+                typeof options.description === "string" && options.description.length
+                ))
+                throw new Meteor.Error(400, "Required parameter missing");
+            if (options.title.length > 100)
+                throw new Meteor.Error(413, "Event name too long");
+            if (options.location.length > 100)
+                throw new Meteor.Error(413, "Location too long");
+            if (options.artifact.length > 100)
+                throw new Meteor.Error(413, "Item name too long");
+            if (options.category.length > 100)
+                throw new Meteor.Error(413, "Category too long");
+            if (options.description.length > 2000)
+                throw new Meteor.Error(413, "Description too long");
+            //if (! this.userId)
+            //    throw new Meteor.Error(403, "You must be logged in");
+            return Events.insert({
+                owner: "Mordka",
+                latlng: options.latlng,
+                title: options.title,
+                location: options.location,
+                artifact: options.artifact,
+                category: options.category,
+                description: options.description,
+                datePublished: [],
+                dateEvent: []
+            });
+        }
+    })
 }
 
 
