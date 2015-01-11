2875490f3acf1ed1c443525a724ae5bd10872939
mordka <pawel.smile@gmail.com>
2015-01-11 16:26:49 +0000

    search fix

--- client/search.js
+++ client/search.js
@@ -6,9 +6,10 @@ Template.results.helpers({
 
 Template.banner.events({
     "submit .search-events": function (event) {
+        event.preventDefault();
         var search_value = event.target.search.value;
         if (!search_value) return false;
-        Session.set('results', Events.find({title: keyword}));
+        Session.set('results', Events.find({title: search_value}));
         event.target.search.value = "";
         return false;
     }
