a911f7efdc08ffff425adba6588d6bdbfdda31c4
mordka <pawel.smile@gmail.com>
2015-04-14 18:41:11 +0100

    environment variables added

--- server/boot.js
+++ server/boot.js
@@ -3,10 +3,22 @@ switch (process.env.ROOT_URL) {
     case "http://localhost:3000/":
         process.env.MEETUP_CLIENT_ID = "38k5c02dj52h5mga9k321ud3uq";
         process.env.MEETUP_SECRET = "s9b2b3tdk6dmfng9vsmuqi5jjj";
+        process.env.FACEBOOK_CLIENT_ID = "392906830890754";
+        process.env.FACEBOOK_SECRET = "bebe3ceb92be8ec14a363ff325af9a49";
+        process.env.GOOGLE_CLIENT_ID = "999855433817-0n06f7rotm4nq1lkdeq7850fde2cin7h.apps.googleusercontent.com";
+        process.env.GOOGLE_SECRET = "nOOTfPCRL8SnntlE6tUeEam8";
+        process.env.TWITTER_CLIENT_ID = "da4r5pUWytrdy1ztR6bDfvW5U";
+        process.env.TWITTER_SECRET = "oS0KtY1WsGv4yt3fSz6UbJVFXB1riAr5aVLngdOZl9sX9Fx2Je";
         break;
     case "http://focallocal.meteor.com/":
         process.env.MEETUP_CLIENT_ID = "r2p02hica9uktu1uqpk1d184o9";
         process.env.MEETUP_SECRET = "i7605716jmpbef8o2kumlo70cj";
+        process.env.FACEBOOK_CLIENT_ID = "392906830890754";
+        process.env.FACEBOOK_SECRET = "bebe3ceb92be8ec14a363ff325af9a49";
+        process.env.GOOGLE_CLIENT_ID = "999855433817-0n06f7rotm4nq1lkdeq7850fde2cin7h.apps.googleusercontent.com";
+        process.env.GOOGLE_SECRET = "nOOTfPCRL8SnntlE6tUeEam8";
+        process.env.TWITTER_CLIENT_ID = "da4r5pUWytrdy1ztR6bDfvW5U";
+        process.env.TWITTER_SECRET = "oS0KtY1WsGv4yt3fSz6UbJVFXB1riAr5aVLngdOZl9sX9Fx2Je";
         break;
 }
 
@@ -14,8 +26,8 @@ ServiceConfiguration.configurations.upsert(
     {"service": "facebook"},
     {
         $set: {
-            "appId": "392906830890754",
-            "secret": "bebe3ceb92be8ec14a363ff325af9a49",
+            "appId": process.env.FACEBOOK_CLIENT_ID,
+            "secret": process.env.FACEBOOK_SECRET,
             "loginStyle": "popup"
         }
     });
@@ -24,8 +36,8 @@ ServiceConfiguration.configurations.upsert(
     {"service": "google"},
     {
         $set: {
-            "clientId": "999855433817-0n06f7rotm4nq1lkdeq7850fde2cin7h.apps.googleusercontent.com",
-            "secret": "nOOTfPCRL8SnntlE6tUeEam8",
+            "clientId": process.env.GOOGLE_CLIENT_ID,
+            "secret": process.env.GOOGLE_SECRET,
             "loginStyle": "popup"
         }
     });
@@ -35,8 +47,8 @@ ServiceConfiguration.configurations.upsert(
     {"service": "twitter"},
     {
         $set: {
-            "consumerKey": "da4r5pUWytrdy1ztR6bDfvW5U",
-            "secret": "oS0KtY1WsGv4yt3fSz6UbJVFXB1riAr5aVLngdOZl9sX9Fx2Je",
+            "consumerKey": process.env.TWITTER_CLIENT_ID,
+            "secret": process.env.TWITTER_SECRET,
             "loginStyle": "popup"
         }
     });
