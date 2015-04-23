a5281f7a586e2d768d4a4335a2589e758e1a21e2
mordka <pawel.smile@gmail.com>
2015-04-23 08:13:08 +0100

    added icon to submit button positioned ADD EVENT button and created same button to go back on new event page

--- client/templates/itemsNew/itemsNew.js
+++ client/templates/itemsNew/itemsNew.js
@@ -1,8 +1,8 @@
 AutoForm.hooks({
-  'items-new-form': {
-    onSuccess: function (operation, result, template) {
-      Materialize.toast('Item created successfully!', 4000);
-      Router.go('dashboard');
+    'items-new-form': {
+        onSuccess: function(operation, result, template) {
+            Materialize.toast('Item created successfully!', 4000);
+            Router.go('dashboard');
+        }
     }
-  }
 });
