29030326a052cf2ad474d59d4a951f4e2ac4550e
mordka <pawel.smile@gmail.com>
2015-05-11 09:07:32 +0100

    styled events colors

--- server/seeds/categories.js
+++ server/seeds/categories.js
@@ -1,16 +1,16 @@
 Meteor.startup(function() {
 if (Categories.find({}).count() === 0) {
-    Categories.upsert({"_id": 1}, {$set: {"name": "Pillow Fight 4Connection", "color": '#E42B33'}});
-    Categories.upsert({"_id": 2}, {$set: {"name": "Take a Smile", "color": '#DBDB00'}});
-    Categories.upsert({"_id": 3}, {$set: {"name": "Give a Bubble", "color": '#E9A743'}});
-    Categories.upsert({"_id": 4}, {$set: {"name": "Free Hugs", "color": '#CD36EC'}});
-    Categories.upsert({"_id": 5}, {$set: {"name": "Musical Connection", "color": '#05C7F2'}});
-    Categories.upsert({"_id": 6}, {$set: {"name": "Connecting Canvas", "color": '#E9A743'}});
-    Categories.upsert({"_id": 7}, {$set: {"name": "Inspire YOUR City", "color": '#FFF13B'}});
-    Categories.upsert({"_id": 8}, {$set: {"name": "Guerrilla Urban Beautification", "color": '#1AF23A'}});
-    Categories.upsert({"_id": 9}, {$set: {"name": "Lets Talk about Togetherness", "color": '#84EC9E'}});
-    Categories.upsert({"_id": 10}, {$set: {"name": "Community Slip n Slide", "color": '#7F31E4'}});
-    Categories.upsert({"_id": 11}, {$set: {"name": "Togetherness Projects", "color": '#BE93DB'}});
+    Categories.upsert({"_id": 1}, {$set: {"name": "Pillow Fight 4Connection", "color": '#d50000'}});
+    Categories.upsert({"_id": 2}, {$set: {"name": "Take a Smile", "color": '#33691e'}});
+    Categories.upsert({"_id": 3}, {$set: {"name": "Give a Bubble", "color": '#304ffe'}});
+    Categories.upsert({"_id": 4}, {$set: {"name": "Free Hugs", "color": '#c51162'}});
+    Categories.upsert({"_id": 5}, {$set: {"name": "Musical Connection", "color": '#0091ea'}});
+    Categories.upsert({"_id": 6}, {$set: {"name": "Connecting Canvas", "color": '#ff3d00'}});
+    Categories.upsert({"_id": 7}, {$set: {"name": "Inspire YOUR City", "color": '#ff6d00'}});
+    Categories.upsert({"_id": 8}, {$set: {"name": "Guerrilla Urban Beautification", "color": '#00c853'}});
+    Categories.upsert({"_id": 9}, {$set: {"name": "Lets Talk about Togetherness", "color": '#aeea00'}});
+    Categories.upsert({"_id": 10}, {$set: {"name": "Community Slip n Slide", "color": '#6200ea'}});
+    Categories.upsert({"_id": 11}, {$set: {"name": "Togetherness Projects", "color": '#00bfa5'}});
     console.log("categories updated " + Categories.find({}).count());
 }
 });
