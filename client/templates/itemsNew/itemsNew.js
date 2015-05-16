1c08c97c05290a0b04bed82a16ff94edb12a56c1
mordka <pawel.smile@gmail.com>
2015-05-16 21:56:13 +0100

    refactorization

--- client/templates/itemsNew/itemsNew.js
+++
@@ -1,8 +0,0 @@
-AutoForm.hooks({
-    'items-new-form': {
-        onSuccess: function(operation, result, template) {
-            Materialize.toast('Item created successfully!', 4000);
-            Router.go('calendar');
-        }
-    }
-});
