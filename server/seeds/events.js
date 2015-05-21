e8cf8c9b111c736c611b340bfe7bdb59b8925783
mordka <pawel.smile@gmail.com>
2015-05-21 08:34:59 +0100

    store id of the user when saving form

--- server/seeds/events.js
+++ server/seeds/events.js
@@ -1,25 +1,57 @@
-Meteor.startup(function() {
-Factory.define('event', Events, {
-    coordinates: function() {
-        var location = {"lat":_.random(-90, 90),
-                        "lng":_.random(-180, 180)};
-        return location;
-    },
-    organiser: function() { return Fake.user().name; },
-    location: function() { return Fake.word(); },
-    name: function() { return Fake.word(); },
-    meetingPoint: function() { return Fake.word(); },
-    url: function() { return "http://focallocal.org"; },
-    description: function() { return Fake.sentence(); },
-    category: function() { return Categories.findOne({_id : _.random(1,11)}); },
-    dateCreated: function() { return moment().format("YYYY-MM-DD"); },
-    dateEvent: function() { return moment().add(_.random(1,30),'days').format("YYYY-MM-DD"); }
-});
-
-if (Events.find({}).count() === 0) {
-    console.log("Repopulating db...");
-    _(20).times(function (n) {
-        Factory.create('event');
+Meteor.startup(function () {
+    Factory.define('event', Events, {
+        coordinates: function () {
+            var location = {
+                "lat": _.random(-90, 90),
+                "lng": _.random(-180, 180)
+            };
+            return location;
+        },
+        organiser: function () {
+            var userObject = {
+                _id: _.random(1, 11),
+                name: Fake.user().name
+            }
+            return userObject;
+        },
+        location: function () {
+            return Fake.word();
+        }
+        ,
+        name: function () {
+            return Fake.word();
+        }
+        ,
+        meetingPoint: function () {
+            return Fake.word();
+        }
+        ,
+        url: function () {
+            return "http://focallocal.org";
+        }
+        ,
+        description: function () {
+            return Fake.sentence();
+        }
+        ,
+        category: function () {
+            return Categories.findOne({_id: _.random(1, 11)});
+        }
+        ,
+        dateCreated: function () {
+            return moment().format("YYYY-MM-DD");
+        }
+        ,
+        dateEvent: function () {
+            return moment().add(_.random(1, 30), 'days').format("YYYY-MM-DD");
+        }
     });
-}
-});
+
+    if (Events.find({}).count() === 0) {
+        console.log("Repopulating db...");
+        _(20).times(function (n) {
+            Factory.create('event');
+        });
+    }
+})
+;
