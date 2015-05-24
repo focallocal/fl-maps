891638988130d32e9dde8e56cc0f110458d4326e
mordka <pawel.smile@gmail.com>
2015-05-24 08:52:12 +0100

    refactoring: removed items

--- both/controllers/itemsNew.js
+++
@@ -1,3 +0,0 @@
-ItemsNewController = AppController.extend({
-  
-});
--- both/router/routes.js
+++ both/router/routes.js
@@ -6,10 +6,6 @@ Router.route('/events/calendar', {
   name: 'calendar'
 });
 
-Router.route('/items/new', {
-  name: 'items.new'
-});
-
 Router.route('/events/new', {
   name: 'events.new'
 });
