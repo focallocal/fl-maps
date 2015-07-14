630b6ea969f9fe9440aa2d2377c3d8c9e0a59c25
mordka <pawel.smile@gmail.com>
2015-07-14 08:22:40 +0800

    downgrade packages version to fix new bugs: no subscription and visual defects in new event form

--- both/controllers/map.js
+++ both/controllers/map.js
@@ -1,6 +1,6 @@
 MapController = AppController.extend({
     waitOn: function() {
-        return this.subscribe('events');
+        return this.subscribe('events') && this.subscribe('categories');
     },
     data: {
         events: Events.find({dateEvent: {$gte:moment().startOf('day').toDate()}})
