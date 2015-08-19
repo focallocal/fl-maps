a2cb345e1c8b66347f986a569204c97aaf91b9fa
mordka <pawel.smile@gmail.com>
2015-08-19 15:44:38 +0800

    added additional collections to houston admin: users and admins

--- server/seeds/accounts.js
+++ server/seeds/accounts.js
@@ -38,6 +38,10 @@ Meteor.startup(function() {
                 }
             });
     }
+
+    //add additional collections to houston admin
+    Houston.add_collection(Meteor.users);
+    Houston.add_collection(Houston._admins);
 });
 
 Accounts.onCreateUser(function(options, user) {
