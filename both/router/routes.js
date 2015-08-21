2a3f76076bc5266cb4b2f1e1970e935dffabc8c4
mordka <pawel.smile@gmail.com>
2015-08-21 13:46:45 +0200

    fixed routing issues after upgrading packages

--- both/router/routes.js
+++ both/router/routes.js
@@ -2,14 +2,17 @@ Router.route('/', {
   name: 'home'
 });
 
-Router.route('/events/calendar', {
-  name: 'calendar'
+Router.route('/events/list', {
+  name: 'calendar',
+  controller: 'CalendarController'
 });
 
 Router.route('/events/new', {
-  name: 'events.new'
+  name: 'events.new',
+  controller: 'EventsNewController'
 });
 
 Router.route('/events/map', {
-  name: 'map'
+  name: 'map',
+  controller: 'MapController'
 });
