ef7ecec861025258824efc8d6b3fde1355d12eed
mordka <pawel.smile@gmail.com>
2015-09-08 12:53:17 +0200

    implemented basic sharing buttons with suggestions of content

--- client/templates/congratsModal/congratsModal.js
+++ client/templates/congratsModal/congratsModal.js
@@ -1,26 +1,19 @@
 Template.congratsModal.helpers({
-    shareData: function() {
+	selectedEvent: function() {
         var eventId = Session.get('selected');
-        console.log('getting selected event ' + eventId);
         var event = Events.findOne(eventId);  
         if (event == undefined) {
         	Meteor.Error('no-event-selected', 'No event selected.');
         	return;
         }
-        // Template.congratsModal.onCreated();
-        return { 
-        	title: event.name,
-        	author: event.organiser.name,
-        	excerpt: event.description,
-        	url: event.url
-
-        }
+		event.url = event.url ? event.url : window.location.href;
+        return event;
 	},
 	shareOnFacebookLink: function(url) {
 		return 'https://www.facebook.com/sharer/sharer.php?&u=' + url;
 	},
 	shareOnTwitterLink: function(url,title) {
-		return 'https://twitter.com/intent/tweet?url=' + url + '&text=' + title;
+		return 'https://twitter.com/intent/tweet?url=' + url + '&text=' + title ;
 	},
 	shareOnGooglePlusLink: function(url) {
 		return 'https://plus.google.com/share?url=' + url;
