7019f1b1bc2efc9479e38b39148699a758a223cf
mordka <pawel.smile@gmail.com>
2015-09-10 15:54:43 +0200

    resolved conflicts and... - changed orange to the primary color - used Meteor.events instead of JQuery events binding - made search input more Materialize

--- client/templates/calendar/calendar.js
+++ client/templates/calendar/calendar.js
@@ -3,9 +3,22 @@ Template.calendar.helpers({
         return data && data.count() != 0;
     }
 });
+Template.calendar.events({
+    'keyup #search': function(event) {
+        var searchString = event.currentTarget.value.toLowerCase();
 
+        $('.template-calendar li').each(function() {
+            var haystack = $(this).text().toLowerCase();
+            if(searchString.length > 0 && haystack.indexOf(searchString) >= 0) {
+                $(this).addClass('highlight');
+            } else {
+                $(this).removeClass('highlight');
+            }
+        });
+    }
+});
 Template.calendar.rendered=function() {
     $('.collapsible').collapsible({
         accordion : true
     });
-}
+};
