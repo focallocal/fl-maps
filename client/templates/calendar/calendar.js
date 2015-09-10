9409f281947418b81a410fc8e7a3261a347ea7fc
hesslau <hesslau@gmail.com>
2015-09-10 02:35:19 -0700

    adds simple text search to calendar list. matching entries are highlighted

--- client/templates/calendar/calendar.js
+++ client/templates/calendar/calendar.js
@@ -8,4 +8,17 @@ Template.calendar.rendered=function() {
     $('.collapsible').collapsible({
         accordion : true
     });
+
+    $('#search').keyup(function() {
+        var searchString = $(this).val().toLowerCase();
+
+        $('.template-calendar li').each(function() {
+          var haystack = $(this).text().toLowerCase();
+          if(searchString.length > 0 && haystack.indexOf(searchString) >= 0) {
+            $(this).addClass('highlight');
+          } else {
+            $(this).removeClass('highlight');
+          }
+        });
+    });
 }
