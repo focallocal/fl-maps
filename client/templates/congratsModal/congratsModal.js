8b8b959c7e07f4af72a5db5f921f4a50f2a949c9
mordka <pawel.smile@gmail.com>
2015-09-14 23:25:30 +0200

    - added couchsurfing share button - added eventbrite share button - added #Focallocal as default hashtag to Twitter share popup - added 'select all' option

--- client/templates/congratsModal/congratsModal.js
+++ client/templates/congratsModal/congratsModal.js
@@ -1,7 +1,7 @@
 Template.congratsModal.helpers({
 	selectedEvent: function() {
         var eventId = Session.get('selected');
-        var event = Events.findOne(eventId);  
+        var event = Events.findOne(eventId);
         if (event == undefined) {
         	Meteor.Error('no-event-selected', 'No event selected.');
         	return;
@@ -13,9 +13,25 @@ Template.congratsModal.helpers({
 		return 'https://www.facebook.com/sharer/sharer.php?&u=' + url;
 	},
 	shareOnTwitterLink: function(url,title) {
-		return 'https://twitter.com/intent/tweet?url=' + url + '&text=' + title ;
+		return 'https://twitter.com/intent/tweet?url=' + url + '&text=' + title + '&hashtags=Focallocal';
 	},
 	shareOnGooglePlusLink: function(url) {
 		return 'https://plus.google.com/share?url=' + url;
 	}
 });
+function selectText(containerid) {
+			 if (document.selection) {
+					 var range = document.body.createTextRange();
+					 range.moveToElementText(document.getElementById(containerid));
+					 range.select();
+			 } else if (window.getSelection) {
+					 var range = document.createRange();
+					 range.selectNode(document.getElementById(containerid));
+					 window.getSelection().addRange(range);
+			 }
+	 }
+Template.congratsModal.events({
+	'click #copy-button': function() {
+		selectText('selectable');
+	}
+})
