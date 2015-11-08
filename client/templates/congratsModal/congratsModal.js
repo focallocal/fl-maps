82279d9eaa25abeac06b44063605aed39e564201
mordka <pawel.smile@gmail.com>
2015-11-08 19:40:34 +0000

    improve wording and us feed FB api

--- client/templates/congratsModal/congratsModal.js
+++ client/templates/congratsModal/congratsModal.js
@@ -7,10 +7,20 @@ Template.congratsModal.helpers({
         	return;
         }
 		event.url = event.url ? event.url : window.location.href;
-        return event;
+		return event;
 	},
 	shareOnFacebookLink: function(url) {
-		return 'https://www.facebook.com/sharer/sharer.php?&u=' + url;
+		const fbKey = Meteor.settings.public.facebook.oauth_key;
+		var url = 'http://www.facebook.com/dialog/feed?app_id='+ fbKey +
+			'&link=http://focallocal.meteor.com' +
+			'&picture=http://www.lifewithcats.tv/wp-content/uploads/2013/07/xsdre.jpg' +
+			'&name=' + encodeURIComponent('test name') +
+			'&caption=' + encodeURIComponent('test caption') +
+		'&description=' + encodeURIComponent('test description') +
+		'&redirect_uri=http://focallocal.meteor.com' +
+		'&display=popup';
+		return url;
+		//return 'https://www.facebook.com/sharer/sharer.php?&u=' + url;
 	},
 	shareOnTwitterLink: function(url,title) {
 		return 'https://twitter.com/intent/tweet?url=' + url + '&text=' + title + '&hashtags=Focallocal';
@@ -34,4 +44,4 @@ Template.congratsModal.events({
 	'click #copy-button': function() {
 		selectText('selectable');
 	}
-})
+});
