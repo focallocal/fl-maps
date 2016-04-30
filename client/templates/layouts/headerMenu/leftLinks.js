bda7815e55fa9ee94217b6372f9b11114cfd7a8a
mordka <pawel.smile@gmail.com>
2016-04-30 18:03:21 +0100

    fix horizontal view menu

---
+++ client/templates/layouts/headerMenu/leftLinks.js
@@ -0,0 +1,8 @@
+Template.leftLinks.helpers({
+    joinTeamClass: function() {
+        return this.sidenav?'':'hide-on-med-and-down';
+    },
+    liClass: function() {
+        return this.sidenav?'bold':'text-icon'
+    }
+});
