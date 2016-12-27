480e7fa374a3d6aa36d88e30cf3e18c26838fa42
ydaniel98 <ed22333t@gmail.com>
2016-12-27 12:57:24 -0400

    Added Filter (Not Functional) UI for gathers

--- client/templates/calendar/calendar.js
+++ client/templates/calendar/calendar.js
@@ -1,6 +1,6 @@
 Template.calendar.onCreated(function () {
     this.subscribe('events');
-
+    Session.setDefault("filters", []);
 });
 
 Template.calendar.onRendered(function () {
@@ -18,6 +18,9 @@ Template.calendar.helpers({
     },
     pastEvents: function(){
       return Events.find({dateEvent: {$lt:moment().startOf('day').toDate()}}, {sort: {dateEvent: -1}})
+    },
+    filters: function() {
+      return Session.get('filters');
     }
 });
 Template.calendar.events({
@@ -25,12 +28,15 @@ Template.calendar.events({
         var searchString = event.currentTarget.value.toLowerCase();
 
         $('.template-calendar li').each(function() {
-            var haystack = $(this).text().toLowerCase();
+            var $this = $(this);
+            var haystack = $this.text().toLowerCase();
+
             if(searchString.length > 0 && haystack.indexOf(searchString) >= 0) {
-                $(this).addClass('highlight');
+                $this.addClass('highlight');
             } else {
-                $(this).removeClass('highlight');
+                $this.removeClass('highlight');
             }
+
         });
     },
     'click .details-btn': function(event) {
@@ -39,5 +45,26 @@ Template.calendar.events({
         const params = {_id: eventId};
         const path = FlowRouter.path("eventById", params);
         Router.go(path);
+    },
+    'click #gather-filter-btn': function(event) {
+        var $search = $("#search");
+        var filter = $search.val()
+        var filters = Session.get("filters");
+
+        if (filter.length > 0 && filters.indexOf(filter) === -1) {
+          filters.push(filter);
+          Session.set("filters", filters);
+          $search.val('');
+          // 
+          // var events = Events.find({dateEvent: {$gte:moment().startOf('day').toDate()}}, {sort: {dateEvent: 1}});
+          //
+          // console.log(events);
+        }
+    },
+    'click .remove-gather-filter': function(event) {
+        var tag = $(event.currentTarget).parent().text();
+        var filters = Session.get('filters');
+        filters.splice(filters.indexOf(tag.trim()), 1);
+        Session.set("filters", filters);
     }
 });
