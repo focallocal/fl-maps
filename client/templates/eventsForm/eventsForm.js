e0c044f3d16c97781e9eab86ab1023b759181e66
ydaniel98 <ed22333t@gmail.com>
2017-01-05 11:13:17 -0400

    Updated typeahead, removed need for previous fixes

--- client/templates/eventsForm/eventsForm.js
+++ client/templates/eventsForm/eventsForm.js
@@ -20,13 +20,8 @@ AutoForm.hooks({
     }
 });
 
-// Fix for geocodeDataSource (It doesen't have access to Template.instance())
-var templateInstance = null;
-//
-
 Template.eventsForm.onCreated(function() {
     this.debounce = null;
-    templateInstance = Template.instance();
     this.autocompleteMapData = new ReactiveVar([], false);
     console.log('hear');
     this.subscribe('categories');
@@ -38,58 +33,49 @@ Template.eventsForm.onCreated(function() {
 
 });
 
-function fetchTypeaheadData(query, instance) {
-    if (_.isUndefined(query) || query.length < 3) {
-        instance.autocompleteMapData.set([]);
-        return;
-    }
-    if (instance.debounce) {
-        Meteor.clearTimeout(instance.debounce);
-    }
-    const debounceDelay = 500; //wait half a second before triggering search
-    instance.debounce = Meteor.setTimeout(function() {
-        Meteor.call('getCoords', query, function (error, result) {
-            console.info("Query: " + query);
-            var mapResultToDisplay = function () {
-                var isCity = function(element, index) {
-                    return element.city!=null
-                };
-                return result.filter(isCity).map(function (v) {
-                        console.info("Response: " + JSON.stringify(v));
-                        var streetName = _.isNull(v.streetName) ? '' : v.streetName + ' ';
-                        var streetNumber = _.isNull(v.streetNumber) ? _.isEmpty(streetName) ? '' : ', ' : +v.streetNumber + ', ';
-                        var city  = _.isNull(v.city) ? '' : v.city + ', ';
-                        var state  = _.isNull(v.state) ? '' : v.state + ', ';
-                        return {
-                            value: streetName + streetNumber + city + state + v.country,
-                            lat: v.latitude,
-                            lng: v.longitude
-                        };
-                    }
-                );
-            };
-
-            if (error != undefined) {
-                console.error(error);
-                Events.simpleSchema().namedContext("events-form").addInvalidKeys([{
-                    name: "address",
-                    type: "offline"
-                }]);
-            } else {
-                var result = mapResultToDisplay();
-                console.info(result);
-                instance.autocompleteMapData.set(result);
-            }
-        });
-    }, debounceDelay);
-}
-
 Template.eventsForm.helpers({
     categories: function(){
         return Categories.find({});
     },
-    geocodeDataSource: function() {
-        return templateInstance.autocompleteMapData.get();
+    geocodeDataSource: function(query, sync, asyncCallback) {
+        var instance = Template.instance();
+        if (instance.debounce) {
+            Meteor.clearTimeout(instance.debounce);
+        }
+        const debounceDelay = 500; //wait half a second before triggering search
+        instance.debounce = Meteor.setTimeout(function() {
+            Meteor.call('getCoords', query, function (error, result) {
+                console.info("Query: " + query);
+                var mapResultToDisplay = function () {
+                    var isCity = function(element, index) {
+                        return element.city!=null
+                    };
+                    return result.filter(isCity).map(function (v) {
+                            console.info("Response: " + JSON.stringify(v));
+                            var streetName = _.isNull(v.streetName) ? '' : v.streetName + ' ';
+                            var streetNumber = _.isNull(v.streetNumber) ? _.isEmpty(streetName) ? '' : ', ' : +v.streetNumber + ', ';
+                            var city  = _.isNull(v.city) ? '' : v.city + ', ';
+                            var state  = _.isNull(v.state) ? '' : v.state + ', ';
+                            return {
+                                value: streetName + streetNumber + city + state + v.country,
+                                lat: v.latitude,
+                                lng: v.longitude
+                            };
+                        }
+                    );
+                };
+
+                if (error != undefined) {
+                    console.error(error);
+                    Events.simpleSchema().namedContext("events-form").addInvalidKeys([{
+                        name: "address",
+                        type: "offline"
+                    }]);
+                } else {
+                    asyncCallback(mapResultToDisplay());
+                }
+            });
+        }, debounceDelay);
     },
     selectedHandler: function (event, suggestion, datasetName) {
         var coordsDefined = !_.isUndefined(suggestion.lat) && !_.isUndefined(suggestion.lng);
@@ -125,13 +111,6 @@ Template.autoForm.onRendered(function () {
     };
     //this is a hack, because Typeahead duplicates input and inserts it inside of a new span item which breaks Materialize
     fixMaterializeActiveClassTrigger();
-
-    $('input[name=address]').on('input', function() {
-        // Force typeahead to display results
-        $('.typeahead').focus();
-        // Update results from user input
-        fetchTypeaheadData($(this).val(), templateInstance);
-    });
 });
 
 Template.eventsForm.onDestroyed(function () {
