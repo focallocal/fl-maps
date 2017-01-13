42aec6a031ac026bfeec739ccc0de6a8a737b81d
ydaniel98 <ed22333t@gmail.com>
2017-01-13 16:57:26 -0400

    Fixed time format for event details

--- client/helpers/stringHelpers.js
+++ client/helpers/stringHelpers.js
@@ -3,7 +3,7 @@ Template.registerHelper('truncate', function(string, length) {
   return _(cleanString).truncate(length);
 });
 Template.registerHelper('printDate', function(date){
-  return moment(date).format("dddd, MMMM Do YYYY, HH:mm:ss");
+  return moment(date).format("dddd, MMMM Do YYYY, H:mm");
 });
 Template.registerHelper('printDateFromNow', function(date){
   return moment(date).isSame(moment(),'day')?'Today':moment(date).calendar();
