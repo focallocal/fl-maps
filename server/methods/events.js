b180f7434cc5d10cbb52167c3fcf5f186c104e49
ydaniel98 <ed22333t@gmail.com>
2017-02-18 18:06:29 -0400

    Added btn and base code -- not finished

--- server/methods/events.js
+++ server/methods/events.js
@@ -1,5 +1,9 @@
 Meteor.methods({
   'Events.insert': function (params) {
     Events.insert(params);
+  },
+  'Events.update': function(_id, params, options) {
+    console.log(_id);
+    Events.update(_id, params, options);
   }
 });
