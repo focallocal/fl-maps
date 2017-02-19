d32c37a6c6ea7a127b7b7b8004b94cc9e49ffaa0
ydaniel98 <ed22333t@gmail.com>
2017-02-18 20:02:09 -0400

    Added server method

--- server/methods/events.js
+++ server/methods/events.js
@@ -2,8 +2,25 @@ Meteor.methods({
   'Events.insert': function (params) {
     Events.insert(params);
   },
-  'Events.update': function(_id, params, options) {
-    console.log(_id);
-    Events.update(_id, params, options);
+  'Events.report': function(_id) {
+
+    if (_id.length === 0) {
+      return;
+    }
+
+    var event = Events.find({_id: _id});
+    var number = 0;
+
+    if (event.report !== undefined) {
+      number = event.report.number + 1;
+    }
+
+    var report = {
+      status: true,
+      admin_overwrite: false,
+      number: number
+    };
+
+    Events.update(_id, {'$set': {'report': report}}, { upsert: true, validate: false});
   }
 });
