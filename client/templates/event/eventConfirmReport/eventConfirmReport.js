e49ef32aea694538c526290945973aa22de79a0f
ydaniel98 <ed22333t@gmail.com>
2017-02-18 20:02:09 -0400

    Added server method

--- client/templates/event/eventConfirmReport/eventConfirmReport.js
+++ client/templates/event/eventConfirmReport/eventConfirmReport.js
@@ -1,27 +1,10 @@
 Template.eventConfirmReport.events({
     'click .yes-btn': function() {
         var selectedEventId = Session.get('selected');
-        var event = Events.find({_id: selectedEventId}).fetch()[0];
-        var number = event.report;
-
-        if (number) {
-            number = number.number || 0;
-        }
-
-        var report = {
-            status: true,
-            admin_overwrite: false,
-            number: number + 1
-        };
-
-        Meteor.call('Events.update', selectedEventId, {
-            '$set': {
-                'report': report
-            }}, {
-                validate: false
-            });
-
+        Meteor.call('Events.report', selectedEventId);
         GAnalytics.event("Events","report");
         Materialize.toast('Event Reported!', 4000);
+
+        console.log(Events.find({_id: selectedEventId}).fetch()[0]);
     },
 });
