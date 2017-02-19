dac5ae9c4d524000f4cecc54bd3eebe4e8f552d4
ydaniel98 <ed22333t@gmail.com>
2017-02-19 13:39:25 -0400

    Added Schema to Categories

--- both/collections/categories.js
+++ both/collections/categories.js
@@ -1,1 +1,12 @@
 Categories = new Mongo.Collection("categories");
+
+Categories.attachSchema(new SimpleSchema({
+	'name': {
+		type: String
+	},
+	'color': {
+		type: String
+		max: 7,
+		min: 7
+	}
+});
