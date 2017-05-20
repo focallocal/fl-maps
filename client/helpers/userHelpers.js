71ef31399e042d87702cb2f816181addf74821a6
ydaniel98 <ed22333t@gmail.com>
2017-05-20 12:33:18 -0400

    Expanded admin capabilities

--- client/helpers/userHelpers.js
+++ client/helpers/userHelpers.js
@@ -1,4 +1,6 @@
 Template.registerHelper('isCurrentUser', function(organiser){
-    var isDevEnvironment = Meteor.absoluteUrl() == "http://localhost:3000/";
-    return isDevEnvironment ? true : Meteor.user().profile.name == organiser.name;
+    // var isDevEnvironment = Meteor.absoluteUrl() == "http://localhost:3000/";
+    // return isDevEnvironment ? true : Meteor.user().profile.name == organiser.name;
+    var user = Meteor.user();
+    return Roles.userIsInRole(user._id, ['admin']) || user.profile.name == organiser.name;
 });
