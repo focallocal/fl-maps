035a6a460cfbae97ce8f3619a671bffc852ff907
ydaniel98 <ed22333t@gmail.com>
2017-07-27 10:15:09 -0400

    Updated reported events card (godmode)

--- client/templates/godmode/reported/event-info/event-info.js
+++ client/templates/godmode/reported/event-info/event-info.js
@@ -1,3 +1,17 @@
+Template.eventInfo.onCreated(function() {
+	this.engagement = this.data.engagement || {limit: 0, attendees: []};
+	this.deleteClickCount = new ReactiveVar(0);
+});
+
+Template.eventInfo.helpers({
+	attending: function() {
+		return Template.instance().engagement.attendees.length || 0;
+	},
+	limit: function() {
+		return Template.instance().engagement.limit;
+	}
+});
+
 Template.eventInfo.events({
 	'click .collapse-btn': function(event) {
 		var $removeTarget = $(".info-body-active");
@@ -11,5 +25,17 @@ Template.eventInfo.events({
 			}
 			$currentTarget.addClass("info-body-active");
 		});
+	},
+	'click .event-info-delete-btn': function(event, template) {
+		var count = template.deleteClickCount.get() + 1;
+		template.deleteClickCount.set(count);
+
+		if (count === 1) {
+			var $target = $(event.currentTarget);
+			$target.removeClass('black-text');
+			$target.addClass('red-text');
+		} else if (count === 2) {
+			Events.remove(template.data._id);
+		}
 	}
 });
