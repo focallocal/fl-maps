d3d0e7b8b4008d2da31df542dcdb156a49ab35d3
Huascar <huascarmc@hotmail.com>
2018-05-19 17:21:37 +0100

    created SmileyLogo component

--- imports/client/ui/pages/Home/SecondSection/About/index.js
+++ imports/client/ui/pages/Home/SecondSection/About/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { Meteor } from 'meteor/meteor'
 import { withTracker } from 'meteor/react-meteor-data'
 import i18n from '/imports/both/i18n/en'
+import SmileyLogo from './Items/SmileyLogo'
 import './styles.scss'
 
 const { Home } = i18n
@@ -10,34 +11,31 @@ class About extends Component {
     return (
       <section id="about" className="content-section">
         <div>
-          <h2>What is Focallocal?</h2>
+          <h2>{Home.second_section.title}</h2>
           <div>
             <p>
-              Focallocal is an open, welcoming community of thousands of positive people around the world.
+              {Home.second_section.content.first}
             </p>
             <p>
-              Since 2011 we have been coming together and targeting the root causes of any, and all societal issues.
+              {Home.second_section.content.second}
             </p>
             <p>
-              Sharing ideas, and using fun activities to empower people to build a friendlier, happier more connected future for everyone.
+            {Home.second_section.content.third}
             </p>
           </div>
           <div className="items">
-            <div className="item">
-              <div className="icon smiley" />
-              <h4>make the world ‘happier’ every day</h4>
-            </div>
+            <SmileyLogo />
             <div className="item">
               <div className="icon friends" />
-              <h4>make ‘positive’ friends all over the world</h4>
+              <h4>{Home.second_section.content.items.friends}</h4>
             </div>
             <div className="item">
               <div className="icon share" />
-              <h4>share or learn skills to </h4>
+              <h4>{Home.second_section.content.items.share} </h4>
             </div>
           </div>
           <p>
-            <span>Join us and turn Positive Action into a daily habit to explore your power to change the world, and the happiness it'll bring into your life</span>
+            <span>{Home.second_section.content.fourth}</span>
           </p>
         </div>
       </section>
