dfa2bb7dcad947be2ed6e68cfb51df883d02c6ca
Huascar <huascarmc@hotmail.com>
2018-05-20 20:28:50 +0100

    replaced strings with i18n

--- imports/client/ui/pages/Home/ThirdSection/Adventures/index.js
+++ imports/client/ui/pages/Home/ThirdSection/Adventures/index.js
@@ -2,30 +2,30 @@ import React, { Component } from 'react'
 import i18n from '/imports/both/i18n/en'
 import './styles.scss'
 
-// const { Home } = i18n
+const { Home } = i18n
 class Adventures extends Component {
   render () {
     return (
       <template name="adventures">
         <section id="adventures" className="content-section side-text">
           <div className="container">
-            <h2>What adventures await me?</h2>
+            <h2>{Home.third_section.title}</h2>
             <div className="row">
               <div>
                 <iframe width="100%" height="315" src="https://www.youtube.com/embed/tG4l514O0t4" frameBorder="0" allowFullScreen></iframe>
               </div>
               <div className="text-cont">
                 <p>
-                  You'll be a part of a growing global Movement with a plan to change the world
+                  {Home.third_section.content.first}
                 </p>
                 <p>
-                  Meeting positive, pro-active, awesome new friends both locally, and globally
+                  {Home.third_section.content.second}
                 </p>
                 <p>
-                  Together using simple, fun activities and exploring new ideas to shape friendlier, happier communities
+                  {Home.third_section.content.third}
                 </p>
                 <p>
-                  Bringing peace and well-being to everyone, everywhere
+                  {Home.third_section.content.fourth}
                 </p>
               </div>
             </div>
--- imports/client/ui/pages/Home/__tests__/ThirdSection.test.js
+++ imports/client/ui/pages/Home/__tests__/ThirdSection.test.js
@@ -20,78 +20,103 @@ describe('<Adventures />', () => {
     expect(component.exists()).toBeTruthy()
   })
 
-  it('should render an <Adventures /> component', () => {
+  it('should render component', () => {
     expect(component.find(Adventures)).toHaveLength(1)
   })
 
-  it('should render <Adventures /> template HTML tags', () => {
+  it('should render template HTML tags', () => {
     expect(component.find('template')).toHaveLength(1)
   })
 
-  it('should render <Adventures /> template HTML tags', () => {
+  it('should render template HTML tags', () => {
     expect(component.find('section')).toHaveLength(1)
   })
 
-  it('should render <Adventures /> with h2 tags', () => {
-    const about = component.find(Adventures)
-    expect(about.find('h2')).toHaveLength(1)
+  it('should render with h2 tags', () => {
+    const adventures = component.find(Adventures)
+    expect(adventures.find('h2')).toHaveLength(1)
   })
 
-  // it('should render <Adventures /> with second_section title', () => {
-  //   const about = component.find(Adventures)
-  //   expect(about.find('h2').text()).toBe(Home.second_section.title)
-  // })
+  it('should render with third_section title', () => {
+    const adventures = component.find(Adventures)
+    expect(adventures.find('h2').text()).toBe(Home.third_section.title)
+  })
+
+  it('should render with four p tags', () => {
+    const adventures = component.find(Adventures)
+    expect(adventures.find('p').length).toBe(4)
+  })
+
+  it('should render with third_section content first', () => {
+    const adventures = component.find(Adventures)
+    const actual = adventures.find('p').at(0)
+    expect(actual.text()).toBe(Home.third_section.content.first)
+  })
+
+  it('should render with third_section content second', () => {
+    const adventures = component.find(Adventures)
+    const actual = adventures.find('p').at(1)
+    expect(actual.text()).toBe(Home.third_section.content.second)
+  })
+
+  it('should render with third_section content third', () => {
+    const adventures = component.find(Adventures)
+    const actual = adventures.find('p').at(2)
+    expect(actual.text()).toBe(Home.third_section.content.third)
+  })
+
+  it('should render with third_section content fourth', () => {
+    const adventures = component.find(Adventures)
+    const actual = adventures.find('p').at(3)
+    expect(actual.text()).toBe(Home.third_section.content.fourth)
+  })
+
   //
-  // it('should render <Adventures /> with four p tags', () => {
-  //   const about = component.find(Adventures)
-  //   expect(about.find('p').length).toBe(4)
-  // })
-  //
-  // it('should render <Adventures /> with second_section content one', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find('p').at(0)
+  // it('should render  with second_section content one', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find('p').at(0)
   //   expect(actual.text()).toBe(Home.second_section.content.first)
   // })
   //
-  // it('should render <Adventures /> with second_section content two', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find('p').at(1)
+  // it('should render  with second_section content two', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find('p').at(1)
   //   expect(actual.text()).toBe(Home.second_section.content.second)
   // })
   //
-  // it('should render <Adventures /> with second_section content three', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find('p').at(2)
+  // it('should render  with second_section content three', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find('p').at(2)
   //   expect(actual.text()).toBe(Home.second_section.content.third)
   // })
   //
-  // it('should render <Adventures /> with second_section content four', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find('p').at(3)
+  // it('should render  with second_section content four', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find('p').at(3)
   //   expect(actual.text()).toBe(Home.second_section.content.fourth)
   // })
   //
-  // it('should render <Adventures /> with items div', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find('.items')
+  // it('should render  with items div', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find('.items')
   //   expect(actual.exists()).toBeTruthy()
   // })
 
-  // it('should render <Adventures /> with SmileyLogo', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find(SmileyLogo)
+  // it('should render  with SmileyLogo', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find(SmileyLogo)
   //   expect(actual.exists()).toBeTruthy()
   // })
   //
-  // it('should render <Adventures /> with FriendsLogo', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find(FriendsLogo)
+  // it('should render  with FriendsLogo', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find(FriendsLogo)
   //   expect(actual.exists()).toBeTruthy()
   // })
   //
-  // it('should render <Adventures /> with ShareLogo', () => {
-  //   const about = component.find(Adventures)
-  //   const actual = about.find(ShareLogo)
+  // it('should render  with ShareLogo', () => {
+  //   const adventures = component.find(Adventures)
+  //   const actual = adventures.find(ShareLogo)
   //   expect(actual.exists()).toBeTruthy()
   // })
 })
