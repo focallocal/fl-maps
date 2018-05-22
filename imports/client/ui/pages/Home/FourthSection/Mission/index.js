fbc30b647751298ec9e74f4351c1b97dbc8884f8
Huascar <huascarmc@hotmail.com>
2018-05-22 13:45:29 +0100

    fourth-section + test-suite set-up, render test passed

---
+++ imports/client/ui/pages/Home/FourthSection/Mission/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import i18n from '/imports/both/i18n/en'
+import './styles.scss'
+
+// const { Home } = i18n
+class Mission extends Component {
+  render () {
+    return (
+      <section id="adventures" className="content-section">
+        <div>
+
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Mission
