c69fb45713f1e18e461f073b28bb969d22234b55
Huascar <huascarmc@hotmail.com>
2018-05-24 19:45:08 +0100

    set-up fifthSection with test suite, render test passed

---
+++ imports/client/ui/pages/Home/FifthSection/MoreLinks/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+// import i18n from '/imports/both/i18n/en'
+import './styles.scss'
+
+// const { Home } = i18n
+class MoreLinks extends Component {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default MoreLinks
