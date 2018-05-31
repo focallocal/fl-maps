d7d675c97a954d7bd85a7b26851511903b0cc72d
roy <roygiladix@gmail.com>
2018-05-31 18:29:18 +0300

    removed logo

--- imports/client/ui/pages/Home/FirstSection/index.js
+++ imports/client/ui/pages/Home/FirstSection/index.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import { Container } from 'reactstrap'
 import FindOrPost from './FindOrPost'
 import './styles.scss'
 
 const FirstSection = () => (
   <section id='first-section'>
     <Container>
-      <Row>
-        <Col>
-          <img src='/images/logo.png' className='logo'/>
-        </Col>
-
-        <Col>
-          <FindOrPost />
-        </Col>
-      </Row>
+      <FindOrPost />
     </Container>
   </section>
 )
