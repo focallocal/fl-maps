3c44ade0db6637fac1b1a7b82ac97a8725deb61c
roy <roygiladix@gmail.com>
2018-06-08 06:00:48 +0300

    revert menu [skip ci]

--- imports/client/ui/components/ScrollToTop/index.js
+++ imports/client/ui/components/ScrollToTop/index.js
@@ -3,7 +3,6 @@ import { withRouter } from 'react-router-dom'
 
 class ScrollToTop extends Component {
   componentDidUpdate (prevProps) {
-    console.log(this.props.location, prevProps.location)
     if (this.props.location !== prevProps.location) {
       window.scrollTo(0, 0)
     }
