1df192fc7ae9c7eed7c8ed08dac51db90fa01428
roy <roygiladix@gmail.com>
2018-06-12 18:14:38 +0300

    update logo

--- imports/client/ui/includes/MainMenu/Logo/index.js
+++ imports/client/ui/includes/MainMenu/Logo/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { NavbarBrand } from 'reactstrap'
+import i18n from '/imports/both/i18n/en'
 import './styles.scss'
 
 const Logo = () => (
   <NavbarBrand id='brand-logo' tag='div'>
-    <NavLink to='/' exact>Focallocal</NavLink>
+    <NavLink to='/' exact>{i18n.MainMenu.logo}</NavLink>
   </NavbarBrand>
 )
 
