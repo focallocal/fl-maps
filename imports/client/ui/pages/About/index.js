4536774b65f9cc7a64011ad2196963c7473c1205
roy <roygiladix@gmail.com>
2018-07-04 00:25:56 +0300

    sync with master

--- imports/client/ui/pages/About/index.js
+++ imports/client/ui/pages/About/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import FirstSection from './FirstSection'
 import SecondSection from './SecondSection'
+import AboutSection from '../Home/SecondSection'
 import './styles.scss'
 
 class About extends Component {
@@ -13,6 +14,7 @@ class About extends Component {
       <div id='about'>
         <h2>About Us</h2>
         <div className='header-divider' />
+        <AboutSection button= {false} />
         <FirstSection />
         <SecondSection />
       </div>
