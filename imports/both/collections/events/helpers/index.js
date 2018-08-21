0399a921a21af1b4b06a22a94c9aebcec083c32c
AndyatFocallocal <focallocal.london@gmail.com>
2018-08-21 21:05:14 +0100

    updated label 'from' and 'until'

--- imports/both/collections/events/helpers/index.js
+++ imports/both/collections/events/helpers/index.js
@@ -5,7 +5,7 @@ export const startingTime = {
   allowedValues: possibleEventHours,
   uniforms: {
     'customType': 'select',
-    'label': 'From'
+    'label': 'Times'
   }
 }
 
@@ -14,7 +14,7 @@ export const endingTime = {
   allowedValues: possibleEventHours,
   uniforms: {
     'customType': 'select',
-    'label': 'Until'
+    'label': ' '
   }
 }
 
