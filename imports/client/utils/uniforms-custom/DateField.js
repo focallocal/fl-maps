2ca5bfd8dcec6b145f45e7fe22584d17c1febdd0
badpointer <arindil@gmail.com>
2018-10-20 21:30:04 -0500

    Fixed issue where values are not being set when a user picks a date

--- imports/client/utils/uniforms-custom/DateField.js
+++ imports/client/utils/uniforms-custom/DateField.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
-import connectField from "uniforms/connectField";
-import DayPicker from "react-day-picker/DayPicker";
-import { FormGroup, Label, Input, Modal } from "reactstrap";
+import React, { Component } from 'react'
+import connectField from 'uniforms/connectField'
+import DayPicker from 'react-day-picker/DayPicker'
+import { FormGroup, Label, Input, Modal } from 'reactstrap'
 
-const currentYear = new Date().getFullYear();
-const fromMonth = new Date(currentYear, 0);
-const toMonth = new Date(currentYear + 10, 11);
+const currentYear = new Date().getFullYear()
+const fromMonth = new Date(currentYear, 0)
+const toMonth = new Date(currentYear + 10, 11)
 
-function YearMonthForm({ date, localeUtils, onChange }) {
-  const months = localeUtils.getMonths();
+function YearMonthForm ({ date, localeUtils, onChange }) {
+  const months = localeUtils.getMonths()
 
-  const years = [];
+  const years = []
   for (let i = fromMonth.getFullYear(); i <= toMonth.getFullYear(); i += 1) {
-    years.push(i);
+    years.push(i)
   }
 
-  const handleChange = function handleChange(e) {
-    const { year, month } = e.target.form;
-    onChange(new Date(year.value, month.value));
-  };
+  const handleChange = function handleChange (e) {
+    const { year, month } = e.target.form
+    console.log('Handle Change', [year.value, month.value])
+    onChange(new Date(year.value, month.value))
+  }
 
   return (
     <form className="DayPicker-Caption">
@@ -37,28 +38,28 @@ function YearMonthForm({ date, localeUtils, onChange }) {
         ))}
       </select>
     </form>
-  );
+  )
 }
 
 class Date_ extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
-    this.handleYearMonthChange = this.handleYearMonthChange.bind(this);
-    this.toggleDayPicker = this.toggleDayPicker.bind(this);
+    this.handleYearMonthChange = this.handleYearMonthChange.bind(this)
+    this.toggleDayPicker = this.toggleDayPicker.bind(this)
     this.state = {
       showDayPicker: false,
       month: new Date(currentYear, new Date().getMonth())
-    };
+    }
   }
 
-  handleYearMonthChange(month) {
-    this.setState({ month });
+  handleYearMonthChange (month) {
+    this.setState({ month })
   }
 
-  render() {
-    const { id, label, onChange, value, error, placeholder } = this.props;
-    const { showDayPicker } = this.state;
+  render () {
+    const { id, label, onChange, value, error } = this.props
+    const { showDayPicker } = this.state
 
     return (
       <FormGroup className="date-field">
@@ -68,7 +69,7 @@ class Date_ extends Component {
           value={formatDate(value)}
           onFocus={e => this.toggleDayPicker(e, true)}
           invalid={Boolean(error)}
-          onChange={() => {}}
+          onChange={onChange} // <------Input isn't being changed.
         />
         <Modal
           isOpen={showDayPicker}
@@ -93,21 +94,21 @@ class Date_ extends Component {
           </div>
         </Modal>
       </FormGroup>
-    );
+    )
   }
 
   handleChange = (date = null) => {
-    this.toggleDayPicker();
-    this.props.onChange(date);
+    this.toggleDayPicker()
+    this.props.onChange(date)
   };
 
   clearDate = () => {
-    this.toggleDayPicker();
-    this.props.onChange(null);
+    this.toggleDayPicker()
+    this.props.onChange(null)
   };
 
   toggleDayPicker = () => {
-    this.setState(prevState => ({ showDayPicker: !prevState.showDayPicker }));
+    this.setState(prevState => ({ showDayPicker: !prevState.showDayPicker }))
   };
 }
 
@@ -116,11 +117,11 @@ const formatDate = date => {
     return date
       .toISOString()
       .substring(0, 10)
-      .split("-")
-      .join("/");
+      .split('-')
+      .join('/')
   } catch (ex) {
-    return "";
+    return ''
   }
-};
+}
 
-export default connectField(Date_);
+export default connectField(Date_)
