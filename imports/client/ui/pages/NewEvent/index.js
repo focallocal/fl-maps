9ebdc472bf55bc1473eace9dc59504b3a591877b
agentboo <zuzi.han@gmail.com>
2018-10-30 03:14:24 -0400

    copied unfinished New Event to global window for later reuse in FormWizard + changed modal redirect to /home on any modal closing

--- imports/client/ui/pages/NewEvent/index.js
+++ imports/client/ui/pages/NewEvent/index.js
@@ -6,6 +6,7 @@ import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Alert } from 'react
 import FormWizard from './FormWizard'
 import i18n from '/imports/both/i18n/en'
 import qs from 'query-string'
+import cloneDeep from 'clone-deep'
 import './styles.scss'
 
 const { NewEventModal: i18n_ } = i18n // Strings from i18n
@@ -44,6 +45,12 @@ class NewEventModal extends Component {
         this.setState({ googleLoaded: true })
       }
     }, 1000) // 1 second
+  }
+
+  componentDidUpdate(prevProps){
+    if(prevProps.location.pathname !== this.props.location.pathname){
+      delete window.__unfinishedNewEvent
+    }
   }
 
   componentWillUnmount () {
@@ -119,6 +126,9 @@ class NewEventModal extends Component {
 
         if (Meteor.isDevelopment) { console.log(err.details, err) }
       })
+
+      // get rid of any previously unfinished New Event
+      delete window.__unfinishedNewEvent
   }
 
   callNewEvent = model => {
@@ -156,6 +166,10 @@ class NewEventModal extends Component {
 
     const url = pathname + '?' + qs.stringify(queryStrings)
     this.props.history.push(url)
+
+    // toggleModal() closes modal, but it is not called after form submits  
+    // copy unfinished form to global window and check for it inside FormWizard 
+    window.__unfinishedNewEvent = cloneDeep(this.state.form.getModel())
   }
 
   toggleErrors = () => this.setState({ hasErrors: false })
