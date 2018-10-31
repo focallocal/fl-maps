2743f7a945c35dd10a934edde1099c5201dcd056
agentboo <zuzi.han@gmail.com>
2018-10-30 21:46:06 -0400

    adjusted styles for FormWizard and changed style on reset Button

--- imports/client/ui/pages/NewEvent/index.js
+++ imports/client/ui/pages/NewEvent/index.js
@@ -83,7 +83,9 @@ class NewEventModal extends Component {
             editMode={editMode} />
         </ModalBody>
         <Alert color='danger' isOpen={hasErrors} toggle={this.toggleErrors} className='error-general'>
-          Please check that you've filled all the necessary fields
+          Please check that <strong>all the necessary fields</strong> 
+          (outlined in <strong>red</strong>) 
+          <strong> are filled </strong>. 
         </Alert>
         <ModalFooter>
           {currentStep + 1 <= 1 &&
