8ad98493173284d17062e4222cd1518e428d45ca
Yohanes Kurnia <yohanes.kurnia@hotmail.com>
2018-12-10 21:04:51 -0800

    Switched position for Attending and Edit Page button. Added colors to variables.scss. Switched button colors

--- imports/client/ui/pages/Page/AttendingButton/index.js
+++ imports/client/ui/pages/Page/AttendingButton/index.js
@@ -1,105 +1,102 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Meteor } from 'meteor/meteor'
-import { Button } from 'reactstrap'
-import { Loader } from '/imports/client/ui/components/PageLoader'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Meteor } from "meteor/meteor";
+import { Button } from "reactstrap";
+import { Loader } from "/imports/client/ui/components/PageLoader";
 
 class AttendingButton extends Component {
   state = {
     updating: false,
     tooManyRequests: false
+  };
+
+  componentDidMount() {
+    this._isMounted = true;
   }
 
-  componentDidMount () {
-    this._isMounted = true
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
-  componentWillUnmount () {
-    this._isMounted = false
-  }
+  render() {
+    const { updating, tooManyRequests } = this.state;
 
-  render () {
-    const {
-      updating,
-      tooManyRequests
-    } = this.state
+    const { _id, isLoggedIn, user } = this.props;
 
-    const {
-      _id,
-      isLoggedIn,
-      user
-    } = this.props
-
-    let isAttending
+    let isAttending;
     if (isLoggedIn) {
-      isAttending = user.attendance ? user.attendance.find(a => a.id === _id) : false
+      isAttending = user.attendance
+        ? user.attendance.find(a => a.id === _id)
+        : false;
     }
 
     return (
-      <div className='attending-button'>
+      <div className="attending-button">
         <Button
-          className={'fixed-bg ' + (isAttending && !updating ? 'active' : '')}
+          className={"fixed-bg " + (isAttending && !updating ? "active" : "")}
           onClick={this.updateAttendance}
           disabled={tooManyRequests}
         >
-          {updating ? (<Loader className='button' />) : (isAttending ? 'Attending!' : 'Attend')}
+          {updating ? (
+            <Loader className="button" />
+          ) : isAttending ? (
+            "Attending!"
+          ) : (
+            "Attend"
+          )}
         </Button>
 
         {tooManyRequests && (
-          <div className='err-msg'>Please wait 10 seconds before trying again</div>
+          <div className="err-msg">
+            Please wait 10 seconds before trying again
+          </div>
         )}
       </div>
-    )
+    );
   }
 
   updateAttendance = () => {
-    const {
-      updating,
-      tooManyRequests
-    } = this.state
+    const { updating, tooManyRequests } = this.state;
 
-    const {
-      _id,
-      user
-    } = this.props
+    const { _id, user } = this.props;
 
     if (!user) {
-      this.redirectToLogin()
+      this.redirectToLogin();
     } else if (!updating && !tooManyRequests) {
       // Fetch with animation in the background, ensure enough time has passed between request/resolve
       // so animation is smooth
-      this.setState({ updating: true })
+      this.setState({ updating: true });
 
-      const startedOn = Date.now()
-      Meteor.call('Events.attendEvent', { id: _id }, (err, res) => {
-        if (err && err.error === 'too-many-requests') {
+      const startedOn = Date.now();
+      Meteor.call("Events.attendEvent", { id: _id }, (err, res) => {
+        if (err && err.error === "too-many-requests") {
           this.setState({
             tooManyRequests: true,
             updating: false
-          })
+          });
 
           setTimeout(() => {
             if (this._isMounted) {
-              this.setState({ tooManyRequests: false })
+              this.setState({ tooManyRequests: false });
             }
-          }, err.details.timeToReset)
+          }, err.details.timeToReset);
         } else {
           // updat state after 1 second so animation can run.
-          const timePassed = Date.now() - startedOn
+          const timePassed = Date.now() - startedOn;
           setTimeout(() => {
             if (this._isMounted) {
-              this.setState({ updating: false, tooManyRequests: false })
+              this.setState({ updating: false, tooManyRequests: false });
             }
-          }, timePassed >= 1000 ? 1 : 1000 - timePassed)
+          }, timePassed >= 1000 ? 1 : 1000 - timePassed);
         }
-      })
+      });
     }
-  }
+  };
 
   redirectToLogin = () => {
-    sessionStorage.setItem('redirect', '/page/' + this.props._id)
-    this.props.history.push('/sign-in')
-  }
+    sessionStorage.setItem("redirect", "/page/" + this.props._id);
+    this.props.history.push("/sign-in");
+  };
 }
 
 AttendingButton.propTypes = {
@@ -107,6 +104,6 @@ AttendingButton.propTypes = {
   history: PropTypes.object.isRequired,
   isLoggedIn: PropTypes.bool.isRequired,
   user: PropTypes.object
-}
+};
 
-export default AttendingButton
+export default AttendingButton;
