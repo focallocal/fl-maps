adb10fc14e5872852755cdc8e9eebae4ac06c5fb
SaudTauqeer <saudtauqeer381@gmail.com>
2019-01-17 23:11:17 +0500

    Added Team page with basic structure.

---
+++ imports/client/ui/pages/TeamMembers/Members.js
@@ -0,0 +1,19 @@
+import React,{Component, Fragment} from "react";
+
+class Members extends Component () {
+    state = {
+        isActiveMember: true
+    }
+    render () {
+        return (
+            <Fragment>
+                Hello there
+            </Fragment>
+        );
+    }
+}
+
+
+
+
+export default Members;
---
+++ imports/client/ui/pages/TeamMembers/index.js
@@ -0,0 +1,9 @@
+//Imports
+import React,{Fragment} from "react";
+import Members from "./Members";
+
+export default () => (
+    <Fragment>
+        <Members />
+    </Fragment>
+);
