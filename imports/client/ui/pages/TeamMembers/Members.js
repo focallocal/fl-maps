cf000f1190b8245352369a16a404901c7cb9a65d
SaudTauqeer <saudtauqeer381@gmail.com>
2019-01-18 15:40:57 +0500

    added social buttons

--- imports/client/ui/pages/TeamMembers/Members.js
+++ imports/client/ui/pages/TeamMembers/Members.js
@@ -1,6 +1,7 @@
 //Imports
 import i18n from "../../../../both/i18n/en";
 import React,{Component, Fragment } from "react";
+import "./team.css";
 import 
 {
 Row,
@@ -17,40 +18,70 @@ Button
 
 const teamData = i18n.Team.members;
 
-
-
 class Members extends Component {
-
-render() {
-  return (
-
-    <Fragment >
-    <div className="mr-5 ml-4">
-    <Row>
-    {teamData.map((member, index)=>{
+  render() {
     return (
-      <Col xs={12} md={3} className="mt-5"> 
-      <Card>
-      <CardImg key={index} width={"100%"} src={member.image} alt={`${member.title} Image`} />
-      <CardBody>
-      <CardTitle> { member.title } </CardTitle>
-      <CardSubtitle>{ member.subtitle }</CardSubtitle>
-      <CardText>{ member.text }</CardText>
-     
-      <Button className="ml-2 mt-2">{ member.title }</Button>
-      <Button className="ml-2 mt-2" >{ member.title }</Button>
-      <Button className="ml-2 mt-2">{ member.title }</Button>
-    
-      </CardBody>
-      </Card>
-      </Col>
-    )
-  })}      
-    </Row>
-    </div>  
-</Fragment>
-  )
+      <Fragment>
+        <div className="mr-5 ml-4">
+          <Row>
+            {teamData.map((member, index) => {
+              return (
+                <Col xs={12} md={3} className="mt-5 mb-4">
+                  <Card>
+                    <CardImg
+                      key={index}
+                      width={"100%"}
+                      src={member.image}
+                      alt={`${member.title} Image`}
+                    />
+                    <CardBody>
+                      <CardTitle> {member.title} </CardTitle>
+                      <CardSubtitle>{member.subtitle}</CardSubtitle>
+                      <CardText>{member.text}</CardText>
+
+                      <section className="contact">
+                        <ul className="list-inline list-social">
+
+                          <li className="list-inline-item social-linked-in">
+                            <a
+                              target="blank"
+                              href={member.linkedIn}
+                            >
+                              <i class="fab fa-linkedin-in" />
+                            </a>
+                          </li>
+
+                          <li className="list-inline-item social-twitter">
+                            <a
+                              target="blank"
+                              href={member.twitter}
+                            >
+                              <i class="fab fa-twitter" />
+                            </a>
+                          </li>
+
+                          <li className="list-inline-item social-github">
+                            <a
+                              target="blank"
+                              href={member.github}
+                            >
+                              <i class="fab fa-github" />
+                            </a>
+                          </li>
+
+                        </ul>
+                      </section>
+                    </CardBody>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-}
+
 export default Members;
