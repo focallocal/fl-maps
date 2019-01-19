2f1b2b22c65ac4fd01aecd58e5ae38ca9291c6d0
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-01-20 04:07:11 +0500

    added faq.json

--- imports/client/ui/pages/Faq/index.js
+++ imports/client/ui/pages/Faq/index.js
@@ -1,15 +1,14 @@
+//Imports
 import React from "react";
-
+import i18n from "../../../../both/i18n/en";
+import { Container } from "reactstrap";
 
 const Index = () => {
   return (
     <React.Fragment>
-      <div>
-        Hello there
-      </div>
+      <Container>{console.log()}</Container>
     </React.Fragment>
   );
-}
-
+};
 
 export default Index;
