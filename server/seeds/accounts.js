d0c915b3b0d6aa40dd15dcbbf21a9f2ec35d1d0e
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-01-24 00:34:59 +0500

    added closing "}"

--- server/seeds/accounts.js
+++ server/seeds/accounts.js
@@ -1,45 +1,46 @@
 /* globals ServiceConfiguration: 1 */
-import { Meteor } from 'meteor/meteor'
+import { Meteor } from "meteor/meteor";
 /*
   Set up oAuth for Google (others except Facebook are TODO)
 */
 
-var settings = Meteor.settings[process.env.NODE_ENV]
+var settings = Meteor.settings[process.env.NODE_ENV];
 if (settings !== undefined) {
   // Google
   ServiceConfiguration.configurations.upsert(
-    { service: 'google' },
+    { service: "google" },
     {
       $set: {
-        'loginStyle': 'popup',
-        'clientId': settings.google.oauth_key,
-        'secret': settings.google.oauth_secret
+        loginStyle: "popup",
+        clientId: settings.google.oauth_key,
+        secret: settings.google.oauth_secret
       }
     }
-  )
+  );
 
-Accounts.onCreateUser(function (options, user) {
-  if (options.profile) user.profile = options.profile
+  Accounts.onCreateUser(function(options, user) {
+    if (options.profile) user.profile = options.profile;
 
-  if (!options.email) {
-    const details = getDetailsFromService(user.services)
-    user.profile = details
-  } else {
-    user.profile = {'name': options.email}
-  }
+    if (!options.email) {
+      const details = getDetailsFromService(user.services);
+      user.profile = details;
+    } else {
+      user.profile = { name: options.email };
+    }
 
-  // Set defaults
-  user.attendance = []
+    // Set defaults
+    user.attendance = [];
 
-  return user
-})
+    return user;
+  });
 
-function getDetailsFromService (services) {
-  const serviceName = 'google'
-  const service = services[serviceName]
+  function getDetailsFromService(services) {
+    const serviceName = "google";
+    const service = services[serviceName];
 
-  return {
-    name: service.name,
-    picture: service.picture
+    return {
+      name: service.name,
+      picture: service.picture
+    };
   }
 }
