c523a09ebb2ee9accb0f9f08188e521cb1cffa66
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-01-24 00:37:17 +0500

    added key values required by react.

--- imports/client/ui/pages/Faq/Content.js
+++ imports/client/ui/pages/Faq/Content.js
@@ -13,8 +13,10 @@ const Content = () => {
       {i18n.Faq.faq.map((item, index) => {
         return (
           <Col className="ml-5 pl-5 mt-5" xs={11}>
-            <h3> {item.heading} </h3>
-            <li className="mb-3 text-left"> {item.text} </li>
+            <h3 key={index}> {item.heading} </h3>
+            <li key={index} className="mb-3 text-left">
+              {item.text}
+            </li>
           </Col>
         );
       })}
