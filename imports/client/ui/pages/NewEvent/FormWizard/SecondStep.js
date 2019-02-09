368ae46b695b512dffae3d5d11a7ef9028cff52e
AndyatFocallocal <focallocal.london@gmail.com>
2019-02-09 22:31:30 +0000

    updated to 1000

--- imports/client/ui/pages/NewEvent/FormWizard/SecondStep.js
+++ imports/client/ui/pages/NewEvent/FormWizard/SecondStep.js
@@ -82,7 +82,7 @@ class SecondStep extends Component {
           type='radio'
         />
         {repeat && <Recurring form={form} />}
-        <AutoField className='pageDetails' name='description' max={800}/>
+        <AutoField className='pageDetails' name='description' max={1000}/>
         <AutoField name='engagement.limit' />
       </div>
     )
