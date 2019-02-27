6c28659b056e2d05ed79bc3e850f04cdea9bec52
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-02-27 21:15:48 +0000

    cherry-pick from master: cb2311d... Rebuilt dateTime UI + util module

--- imports/client/ui/components/HoursFormatted/index.js
+++ imports/client/ui/components/HoursFormatted/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { formatDateWithWords, formatDate } from '/imports/client/utils/format'
+import { findNextEvent, calibrateEndWeekday } from '/imports/client/utils/findNextEvent'
 import './styles.scss'
 
 const HoursFormatted = ({ data }) => {
@@ -9,6 +10,12 @@ const HoursFormatted = ({ data }) => {
     startingTime,
     endingTime
   } = data
+
+  //------------------- Quick Fixes see PR #875 ---------------------
+  const startingDate = startDate === null ? new Date() : startDate
+  //const isSameDay = startingDate.toDateString() === endingDate.toDateString()
+  const isSameDay = startingDate && endingDate ? startingDate.toDateString() === endingDate.toDateString() : true
+  //------------------------------------------------------------------------
 
   if (data.multipleDays) {
     const isEnding = !!endingDate
@@ -38,49 +45,115 @@ const HoursFormatted = ({ data }) => {
       every,
       forever,
       type,
-      repeat,
-      until
+      until,
+      occurences,
+      monthly,
+      recurrenceEndDate
     } = data.recurring
+    
+    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let daysOrdered = days
+    if (days) daysOrdered = weekDays.filter((e) => days.indexOf(e) >= 0)
 
-    // Reusable parts
-    const firstLine = `Every ${every} ${type + (every > 1 ? 's' : '')}`
-    const notForever = (
+    let nextEventInstance = findNextEvent(startingDate, type, every, days, monthly)
+
+    // DESCRIPTION: Reusable Fragment with timestamp on Next Occurence
+    const nextOccurence = (
+      <li className='hours-formatted regular-date'>
+        <span>Next:<br/>
+          {formatDateWithWords(nextEventInstance)}<br/>
+          {startingTime} - </span>
+        {isSameDay ? endingTime
+          : <div>
+            {formatDateWithWords(
+              new Date(Date.parse(nextEventInstance) + (endingDate.getTime() - startingDate.getTime()))
+            )}, {endingTime}
+          </div>
+        }
+      </li>
+    )
+
+    // DESCRIPTION: Reusable Fragment with repetition details
+    const repeatTitle = <span>Repeating:<br/></span>
+    const repeatSchedule = `every ${every > 1 ? every : ''} ${type + (every > 1 ? 's' : '')}`
+    const startingWeekday = startingDate.getDay()
+    let endingWeekday = endingDate.getDay()
+    if (daysOrdered) endingWeekday = weekDays.indexOf(daysOrdered[daysOrdered.length - 1])
+    const adjustedEndDate = calibrateEndWeekday(startingDate, endingDate, days, recurrenceEndDate)
+
+    const notForeverDay = (
       <div className='not-forever'>
-        <span>Available</span>
-        {repeat && ` for ${repeat} occurences`}
-        {(repeat && until) && ` or until ${formatDate(until)}`}
-        {(!repeat && until) && `until ${formatDate(until)}`}
+        {(data.repeat && occurences) && `until ${formatDateWithWords(recurrenceEndDate)}`}
+        {(data.repeat && until) && `until ${formatDateWithWords(until)}`}
+      </div>
+    )
+    const notForeverWeek = (
+      <div className='not-forever'>
+        {(data.repeat && occurences && startingWeekday === endingWeekday)
+          && `until ${formatDateWithWords(recurrenceEndDate)}`}
+        {(data.repeat && occurences && startingWeekday !== endingWeekday)
+          && `until ${formatDateWithWords(adjustedEndDate)}`}
+        {(data.repeat && until) && `until ${formatDateWithWords(until)}`}
+      </div>
+    )
+    const notForeverMonth = (
+      <div className='not-forever'>
+        {(data.repeat && occurences) && `through ${months[recurrenceEndDate.getMonth()]}`}
+        {(data.repeat && until) && `through ${months[until.getMonth()]}`}
       </div>
     )
 
     if (type === 'day') {
       return (
-        <div className='hours-formatted repeat'>
-          <span>{firstLine}, between {startingTime} - {endingTime} </span>
-          {!forever && notForever}
-        </div>
+        <ul className='hours-formatted repeat'>
+          {(forever == true) && nextOccurence}
+          {(!forever && recurrenceEndDate && new Date() < recurrenceEndDate) && nextOccurence}
+          <li>{repeatTitle} {repeatSchedule}, between {startingTime} - {endingTime} </li>
+          {!forever && notForeverDay}
+        </ul>
       )
     } else if (type === 'week') {
       return (
-        <div className='hours-formatted repeat'>
-          <div className='every-sentence'>{firstLine} on</div>
-          {days.map((day, index) => (
-            day &&
-              <div key={index} className='day'>
-                <div>{day.substr(0, 3)}</div>
-                <span>{startingTime} - {endingTime}</span>
-              </div>
-          ))}
-          {!forever && notForever}
-        </div>
+        <ul className='hours-formatted repeat'>
+          {(forever == true) && nextOccurence}
+          {(!forever && recurrenceEndDate && new Date() < recurrenceEndDate) && nextOccurence}
+          <li>
+            <span className='every-sentence'>{repeatTitle} {repeatSchedule}</span><br/>
+            {daysOrdered.map((day, index) => (
+              day &&
+                <div key={index} className='day'>
+                <span>on {day.substr(0, 3)}, {startingTime} - {endingTime}</span><br/>
+                </div>
+            ))}
+          </li>
+          {!forever && notForeverWeek}
+        </ul>
       )
-    } else {
-      // todo - handle "month"
+    } else if (type === 'month'){
+      return (
+        <ul className='hours-formatted repeat'>
+          {nextOccurence}
+          <li>
+            <span className='every-sentence'>{repeatTitle} {repeatSchedule} </span><br/>
+            <span>
+              {(monthly.type === 'byDayInMonth' && monthly.value === 1) && `on the ${monthly.value}st`}
+              {(monthly.type === 'byDayInMonth' && monthly.value === 2) && `on the ${monthly.value}nd`}
+              {(monthly.type === 'byDayInMonth' && monthly.value === 3) && `on the ${monthly.value}rd`}
+              {(monthly.type === 'byDayInMonth' && monthly.value > 3) && `on the ${monthly.value}th`}
+              {(monthly.type === 'byPosition' && monthly.value === 1) && `on the ${monthly.value}st ${weekDays[startingDate.getDay()]}`}
+              {(monthly.type === 'byPosition' && monthly.value === 2) && `on the ${monthly.value}nd ${weekDays[startingDate.getDay()]}`}
+              {(monthly.type === 'byPosition' && monthly.value === 3) && `on the ${monthly.value}rd ${weekDays[startingDate.getDay()]}`}
+              {(monthly.type === 'byPosition' && monthly.value > 3) && `on the ${monthly.value}th ${weekDays[startingDate.getDay()]}`}
+            </span>
+          </li>
+          {!forever && notForeverMonth}
+        </ul>
+      )
     }
   }
 
-  const isSameDay = startingDate.toDateString() === endingDate.toDateString()
-
+  // DESCRIPTION: if !data.repeat then default view is start + end timestamp
   return (
     <div className='hours-formatted regular-date'>
       <span>{formatDateWithWords(startingDate)}, {startingTime} - </span>
