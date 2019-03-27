0496a8693a69868798622ed0f25f82cbfefbe124
AndyatFocallocal <focallocal.london@gmail.com>
2019-03-27 16:26:40 +0100

    adding text

--- imports/client/ui/pages/WhitePaper/index.js
+++ imports/client/ui/pages/WhitePaper/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Container } from "reactstrap";
 import './styles.scss'
 import DCSBalloon from '/imports/client/ui/components/DCSBalloon/index.js'
+<script src="https://unpkg.com/@babel/standalone/babel.js"></script>
+<script src="https://unpkg.com/react/umd/react.development.js"></script>
+<script src="https://unpkg.com/react-dom/umd/react-dom.development.js"></script>
+
 const Remarkable = require('remarkable');
 
 const md = new Remarkable();
@@ -11,108 +15,21 @@ md.set({
 });
 
 
-<h2 className='title'>Welcome to the 1st Draft!</h2>
-<div className="contentspage">
-            <p>Contents<br/>
-The World Needs This Because: (v1)<br/>
-The World Needs This Because: (v2)<br/>
-<br/>
-1. Abstract<br/>
-1.1. Background<br/>
-	1.2. Public Happiness Movement Token Introduction<br/>
-<br/>
-2. Problem Statement <br/>
-	2.1. Issues we aim to solve<br/>
-	2.2. Primary Goal<br/>
-	2.3. Secondary Goal<br/>
-	2.4. Relation to Universal Basic Income<br/>
-<br/>
-3. Educating the Public about Blockchain<br/>
-	3.1. Current State of Public Understanding<br/>
-	3.2. Reaching a New Audience<br/>
-<br/>
-4. The Public Happiness Movement Token<br/>
-	4.1. Token Overview<br/>
-<br/>
-5. Public Happiness Movement Token ICO<br/>
-	5.1. ICO Strategy<br/>
-	5.2. ICO Stages<br/>
-	5.3. Ongoing Funding for Specific Projects and Experts<br/>
-	5.4. ICO Marketing<br/>
-<br/>
-6. Public Happiness – Background<br/>
-	6.1. Community Background<br/>
-	6.2. The Public Happiness Movement Community<br/>
-<br/>
-7. Platform Overview<br/>
-	7.1. The Public Happiness Map (public actions for increasing human and society well-being)<br/>
-	7.2. Action Center (Microvolunteering)<br/>
-	7.3. Community News<br/>
-	7.4. Active Happiness Shop<br/>
-	7.5. Who Defines What is Accepted as ‘Public Happiness’<br/>
-	7.6. Evidence Based and Peer Reviewed Actions<br/>
-	7.7. Who can Participate on the Platform<br/>
-	7.8. Pre-existing Communities with Similar Values<br/>
-	7.9. Languages<br/>
-	7.10. Decentralised Governance and Voting<br/>
-<br/>
-8. The Path to Decentralisation<br/>
-	8.1. Benevolent Dictatorship vs Complete Community Governance<br/>
-	8.2. Example Scenario: Outside Collusion<br/>
-	8.3. Lines of Defence<br/>
-	8.4. Conclusion<br/>
-<br/>
-9. Security
-	9.1. Levels of Trust
-	9.2.. Community Administrators
-	9.3. Project Administrators
-	9.4. Mining Limits
-	9.5. Cold Storage and Key Holders
-
-10. Marketing Strategy
-	10.1. A New Level of Hash tagging
-	10.2. Bus Tour	
-	10.3.. Public Happiness Channel
-	10.4. Happy Cam
-	10.5. Public Happiness Partners
-	10.6. Online Supporters
-	10.7. Contests
-	10.8. Collectables
-
-11.  Automated Token Distribution Matrix
-	11.1. Weighted Mining
-	11.2. Hard Cap
-	11.3. Adjusting the Balancing
-	11.4. Token Release
-	11.5. Mining Table
-	11.6. Experience Multipliers
-	11.7. Token Distribution
-	11.8. Further Matrix Considerations
- 
-12. Team
-	12.1. Current Team Members
-	12.2. Positions Open
-	12.3. Remuneration
-	12.4. Facilities 
- 
-13. Platform Technical's
-	13.1. Crypto Linking Bridges
-	13.2. Trello vs Wekan vs Other Solutions
-	13.3. Leaflet.js vs Google Maps API
-	13.4. Switching the Platform to the Dapps Network
-
-14. Doomsday Protocol
-	14.1. Huge Market Crash
-	14.2. Quantum Security
-	14.3. Massive Hack
-	14.4. Zombie Apocalypse
-
-15. Giving Back to Open-source 
-16. Timeline
-17. White Paper Reviewed By
-18. Questions to be Resolved
- </p>
-  </div>
+<div id="intro"/>
+<script type="text/babel">
+  function whitepaper() {
+    return (
+      <React.Fragment>
+        Some text.
+        <h2>A heading</h2>
+        More text.
+        <h2>Another heading</h2>
+        Even more text.
+      </React.Fragment>
+     );
+  }
+  ReactDOM.render(<Example />, document.getElementById('root'));
+</script>
 
 const markdownSection1 = md.render("
 #Public Happiness Movement Token
@@ -140,6 +57,7 @@ Our community of volunteers has been gradually building a platform to host this 
 It's provides a space for our individual networks to grow into something larger. A global movement for a world with more peace and happiness in it. We’ve reached the point where it is ready to begin hosting communities who will contribute to it governance and improving the platform. You can take a look here: [focallocal.org ](https://focallocal.org)
 
 Note: This white paper is quite long. The most effective way to contribute is by reading the intro and why ‘the world needs this’, and then clicking the sections in the contents page which you’d most like to contribute to. To discuss a section and make suggestions, click on the red balloons.")
+
  
 const markdownSection2 = md.render("INSERT SECTION 2 HERE")
 const markdownSection3 = md.render("INSERT SECTION 3 HERE")
