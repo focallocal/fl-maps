5d9b01cd7ef69244b49789302a078f96018bc2e8
AndyatFocallocal <focallocal.london@gmail.com>
2019-03-27 21:44:08 +0100

    trying our markdown contents page with anchor links

--- imports/client/ui/pages/WhitePaper/index.js
+++ imports/client/ui/pages/WhitePaper/index.js
@@ -65,55 +65,55 @@ The World Needs This Because: (v2)<br/>
 	8.3. Lines of Defence<br/>
 	8.4. Conclusion<br/>
 <br/>
-9. Security
-	9.1. Levels of Trust
-	9.2.. Community Administrators
-	9.3. Project Administrators
-	9.4. Mining Limits
-	9.5. Cold Storage and Key Holders
-
-10. Marketing Strategy
-	10.1. A New Level of Hash tagging
-	10.2. Bus Tour	
-	10.3.. Public Happiness Channel
-	10.4. Happy Cam
-	10.5. Public Happiness Partners
-	10.6. Online Supporters
-	10.7. Contests
-	10.8. Collectables
-
-11.  Automated Token Distribution Matrix
-	11.1. Weighted Mining
-	11.2. Hard Cap
-	11.3. Adjusting the Balancing
-	11.4. Token Release
-	11.5. Mining Table
-	11.6. Experience Multipliers
-	11.7. Token Distribution
-	11.8. Further Matrix Considerations
- 
-12. Team
-	12.1. Current Team Members
-	12.2. Positions Open
-	12.3. Remuneration
-	12.4. Facilities 
- 
-13. Platform Technicals
-	13.1. Crypto Linking Bridges
-	13.2. Trello vs Wekan vs Other Solutions
-	13.3. Leaflet.js vs Google Maps API
-	13.4. Switching the Platform to the Dapps Network
-
-14. Doomsday Protocol
-	14.1. Huge Market Crash
-	14.2. Quantum Security
-	14.3. Massive Hack
-	14.4. Zombie Apocalypse
-
-15. Giving Back to Open-source 
-16. Timeline
-17. White Paper Reviewed By
-18. Questions to be Resolved
+9. Security<br/>
+	9.1. Levels of Trust<br/>
+	9.2.. Community Administrators<br/>
+	9.3. Project Administrators<br/>
+	9.4. Mining Limits<br/>
+	9.5. Cold Storage and Key Holders<br/>
+<br/>
+10. Marketing Strategy<br/>
+	10.1. A New Level of Hash Tagging<br/>
+	10.2. Bus Tour<br/>
+	10.3.. Public Happiness Channel<br/>
+	10.4. Happy Cam<br/>
+	10.5. Public Happiness Partners<br/>
+	10.6. Online Supporters<br/>
+	10.7. Contests<br/>
+	10.8. Collectables<br/>
+<br/>
+11.  Automated Token Distribution Matrix<br/>
+	11.1. Weighted Mining<br/>
+	11.2. Hard Cap<br/>
+	11.3. Adjusting the Balancing<br/>
+	11.4. Token Release<br/>
+	11.5. Mining Table<br/>
+	11.6. Experience Multipliers<br/>
+	11.7. Token Distribution<br/>
+	11.8. Further Matrix Considerations<br/>
+ <br/>
+12. Team<br/>
+	12.1. Current Team Members<br/>
+	12.2. Positions Open<br/>
+	12.3. Remuneration<br/>
+	12.4. Facilities<br/>
+ <br/>
+13. Platform Technicals<br/>
+	13.1. Crypto Linking Bridges<br/>
+	13.2. Trello vs Wekan vs Other Solutions<br/>
+	13.3. Leaflet.js vs Google Maps API<br/>
+	13.4. Switching the Platform to the Dapps Network<br/>
+<br/>
+14. Doomsday Protocol<br/>
+	14.1. Huge Market Crash<br/>
+	14.2. Quantum Security<br/>
+	14.3. Massive Hack<br/>
+	14.4. Zombie Apocalypse<br/>
+<br/>
+15. Giving Back to Open-source<br/>
+16. Timeline<br/>
+17. White Paper Reviewed By<br/>
+18. Questions to be Resolved<br/>
  </p>
   </div>
  </React.Fragment>
@@ -121,8 +121,8 @@ The World Needs This Because: (v2)<br/>
   
 
 const markdownSection1 = md.render(`
-#Public Happiness Movement Token
-#Whitepaper
+# Public Happiness Movement Token
+# Whitepaper
 
 **Foreword:**
 
@@ -134,11 +134,11 @@ GDP was designed by Simon Kuznets, in 1937 as a temporary post war recovery syst
 
 This token is an effort to create a new monetary and economic system. One which focuses energy onto these essential societal areas which are currently overlooked. An effort to create a healthier and happier world.
 
-**Intro:**
 
+**Intro:**
 If the right people get behind this it will have a profound effect on increasing peace, happiness, and reducing human-suffering across the world. If you’re reading this it because we believe that you have the skills to contribute, and a passion to create positive change for humanity.
 
-"Those who love peace must learn to organize as effectively as those who love war"  - Martin Luther King
+> "Those who love peace must learn to organize as effectively as those who love war"  - Martin Luther King
 
 
 Our community of volunteers has been gradually building a platform to host this vision for over 3 years now. The platform is for groups from around the world, anyone who is using positive actions to promote peace and wellbeing through building human and community connection. 
@@ -148,9 +148,27 @@ It's provides a space for our individual networks to grow into something larger.
 Note: This white paper is quite long. The most effective way to contribute is by reading the intro and why ‘the world needs this’, and then clicking the sections in the contents page which you’d most like to contribute to. To discuss a section and make suggestions, click on the red balloons.`)
 
  
-const markdownSection2 = md.render("INSERT SECTION 2 HERE")
-const markdownSection3 = md.render("INSERT SECTION 3 HERE")
-const markdownSection4 = md.render("INSERT SECTION 4 HERE")
+const markdownSection2 = md.render("
+# Markdown contents
+				   
+The World Needs This Because
+1. [Abstract](#test)
+	1.1. [Background](#test2)
+	1.2. [Public Happiness Movement Token Introduction](#test3)
+
+2. Problem Statement 
+	2.1. Issues we aim to solv
+	2.2. Primary Goa
+	2.3. Secondary Goa
+	2.4. Relation to Universal Basic Income
+
+3. Educating the Public about Blockchain
+	3.1. Current State of Public Understanding
+				   ")
+
+const markdownSection3 = md.render("## Some paragraph <a name="test"></a>INSERT SECTION 3 HEre")
+const markdownSection4 = md.render("## Some paragraph <a name="test2"></a>INSERT SECTION 3 HEre"")
+const markdownSection5 = md.render("## Some paragraph <a name="test3"></a>INSERT SECTION 3 HEre"")
 
 const index = (props) => {
   return (
@@ -162,7 +180,9 @@ const index = (props) => {
       <DCSBalloon title="Insert title" subtitle="Insert subtitle" balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
       <div dangerouslySetInnerHTML={{ __html: markdownSection3 }} />
       <DCSBalloon title="Insert title" subtitle="Insert subtitle" balloonId="bal2" display="inline" dcsTags={props.dcsTags} />
-      <div dangerouslySetInnerHTML={{ __html: markdownSection4 }} />       
+      <div dangerouslySetInnerHTML={{ __html: markdownSection4 }} />
+      <DCSBalloon title="Insert title" subtitle="Insert subtitle" balloonId="bal2" display="inline" dcsTags={props.dcsTags} />
+      <div dangerouslySetInnerHTML={{ __html: markdownSection5 }} />      
     </Container>
 
   );
