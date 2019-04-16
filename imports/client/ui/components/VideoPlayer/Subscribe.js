70b7408dc062f4615bd4a71f2d93bb44b94ee8bd
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-04-16 21:44:25 +0100

    duplicate Video Player latest from master, and add jsdoc to package.json

--- imports/client/ui/components/VideoPlayer/Subscribe.js
+++ imports/client/ui/components/VideoPlayer/Subscribe.js
@@ -1,23 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * @author Arty S
+ * Youtube Subscribe Button Component: allows user to subscribe to focallocal channel on click.
+ * works by calling the Google API to render a YT subscribe button
+ * note: requires a link to google's API script to be included in the project's main.html (root) file
+ */
 
 class Subscribe extends Component {
 
-  // NOTE: can only render subscribe button once component has loaded
+  /**
+   * Waits for component to mount before rendering a button
+   */
   componentDidMount() {
-    this.renderYtSubscribeButton('Focallocal', 'default')
+    this.renderYtSubscribeButton('UCkYVBetdxHE5juw7SPMvOGg', 'default')
   }
 
+  /**
+   * Function called on component mount.
+   * grabs the Google API from the window (via main.html root file)
+   * renders the button inside the ytSubscribeBtn container
+   * 
+   * @param {String} channel Youtube channel to subscribe to (provided by componentDidMount()). Can be ID or name.
+   * @param {String} buttonsize size of rendered button default vs full (provided by componentDidMount())
+   */
   renderYtSubscribeButton (channel, buttonsize) {
-    // NOTE: required use of external google api script, this was included in main.html root file
     const googleAPI = window.gapi
     const container = document.getElementById('ytSubscribeBtn');
+    /**
+     * Options passed to Google include the 'layout', and ONE out of 'channelid' OR 'channel' (id vs name)
+     */
     const options = {
-      'channel': channel,
+      'channelid': channel,
       'layout': buttonsize
     };
     googleAPI.ytsubscribe.render(container, options);
   }
 
+  /**
+   * Renders the ytSubscribeBtn container
+   * (this in turn is passed the actual button by Google's API)
+   */
   render() {
     return (
       <div id="ytSubscribeBtn"></div>
