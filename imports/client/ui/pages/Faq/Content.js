1753fc0d0d5a2deb53b96e9eb8035a5e00adc66c
AndyatFocallocal <focallocal.london@gmail.com>
2019-06-12 16:33:35 +0200

    moved balloon

--- imports/client/ui/pages/Faq/Content.js
+++ imports/client/ui/pages/Faq/Content.js
@@ -17,8 +17,8 @@ const Content = (props) => {
         return (
           <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
             <h3>{item.heading} </h3>
-            <li className="mb-3 text-left">{item.text}</li>
             <DCSBalloon title=" " subtitle=" " balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
+            <li className="mb-3 text-left">{item.text}</li>           
           </Col>
         );
       })}
