3709b8a61b1ae40dcd2f4f80985f4d5e9ea76c56
AndyatFocallocal <focallocal.london@gmail.com>
2019-06-12 17:42:55 +0200

    adds content.js

---
+++ imports/client/ui/pages/WhitePaper/Why/Content.js
@@ -0,0 +1,29 @@
+//Imports
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import i18n from "../../../../../both/i18n/en";
+import DCSBalloon from '/imports/client/ui/components/DCSBalloon/index.js'
+
+const Content = (props) => {
+  return (
+    <React.Fragment>
+      <Container className="mt-5">
+        <h1>Looking for answers?</h1>
+        <p>
+          Take a look at frequently asked questions          
+        </p>
+      </Container>
+      {i18n.Whitepaper.Faq.faq.map((item, index) => {
+        return (
+          <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
+            <h3>{item.heading} </h3>
+            <li className="mb-3 text-left">{item.text}</li>
+            <DCSBalloon title=" " subtitle="discuss" balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
+          </Col>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+export default Content;
