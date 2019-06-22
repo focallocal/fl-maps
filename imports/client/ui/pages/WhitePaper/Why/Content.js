52465309f63a6453f6ed33b3a3e6e54f6ef63244
AndyatFocallocal <focallocal.london@gmail.com>
2019-06-22 21:50:06 +0200

    added second section

--- imports/client/ui/pages/WhitePaper/Why/Content.js
+++ imports/client/ui/pages/WhitePaper/Why/Content.js
@@ -9,14 +9,20 @@ const Content = (props) => {
     <React.Fragment>
       <Container className="mt-5">
         <h1>The World Needs a Public Happiness Economy Because:</h1>
+        <p>Short</p>
       </Container>
       {i18n.Whitepaper.Why.faq.map((item, index) => {
         return (
           <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
-            <h3>{item.heading} </h3>
+            <h2>{item.heading} </h2>
             <li className="mb-3 text-left">{item.text}</li>
-            <h2>{item.heading2} </h2>
             <DCSBalloon title=" " subtitle="discuss" balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
+          </Col>
+          <p>Longer</p>
+          <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
+            <p>{item.heading2} </p>
+            <li className="mb-3 text-left">{item.text2}</li>
+            <DCSBalloon title=" " subtitle="discuss" balloonId="bal2" display="inline" dcsTags={props.dcsTags} />
           </Col>
         );
       })}
