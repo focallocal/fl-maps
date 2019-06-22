4d8de554817d7d517302023c83cb8b2d9674db74
AndyatFocallocal <focallocal.london@gmail.com>
2019-06-22 22:01:19 +0200

    added css

--- imports/client/ui/pages/WhitePaper/Why/Content.js
+++ imports/client/ui/pages/WhitePaper/Why/Content.js
@@ -9,16 +9,16 @@ const Content = (props) => {
     <React.Fragment>
       <Container className="mt-5">
         <h1>The World Needs a Public Happiness Economy Because:</h1>
-        <p>Short</p>
+        <h3>Short</h3>
       </Container>
       {i18n.Whitepaper.Why.faq.map((item, index) => {
         return (
           <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
-            <h2>{item.heading} </h2>
+            <h5>{item.heading} </h5>
             <li className="mb-3 text-left">{item.text}</li>
             <DCSBalloon title=" " subtitle="discuss" balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
           </Col>
-          <p>Longer</p>
+          <h3>Longer</h3>
           <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
             <p>{item.heading2} </p>
             <li className="mb-3 text-left">{item.text2}</li>
