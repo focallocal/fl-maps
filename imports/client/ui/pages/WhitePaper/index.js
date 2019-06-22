bf98a04509cf38d9ee783ddf46fc593663063880
AndyatFocallocal <focallocal.london@gmail.com>
2019-06-22 21:31:35 +0200

    removed accidental text at the bottom

--- imports/client/ui/pages/WhitePaper/index.js
+++ imports/client/ui/pages/WhitePaper/index.js
@@ -1761,28 +1761,3 @@ const index = (props) => {
 
   );
 };
-
-export default index;
-Notepad++ v7.7 new features and bug-fixes:
-
-1.  Continue Microsoft binary code signing thanks to the offer from DigiCert (again).
-2.  Upgrade Scintilla from v3.56 to v4.14.
-3.  Fix a regression about memory issue while reloading a file.
-4.  Fix cursor flickering problem after double clicking on URL.
-5.  Make Python files default to using spaces instead of tabs.
-6.  Add "Count in selection" option in Find dialog.
-7.  Add Ctrl + R shortcut for "Reload from disk" command.
-8.  Fix '\' display problem in CSS while using themes (Remove Batang font for CSS tags).
-9.  Fix crash while right clicking on DocSwitcher's column bar.
-10. Fix all plugins being removed problem while Plugin Admin romoves an old plugin (of old system).
-
-
-Included plugins:
-
-1.  NppExport v0.2.8 (32-bit x86 only)
-2.  Converter 4.2.1
-3.  Mime Tool 2.5
-
-Updater (Installer only):
-
-* WinGup (for Notepad++) v5.1
