345fd750376c2514a851ca82628b293901e4bcf3
AndyatFocallocal <focallocal.london@gmail.com>
2019-06-23 19:06:15 +0200

    another try

--- imports/client/ui/pages/WhitePaper/Why/Content.js
+++ imports/client/ui/pages/WhitePaper/Why/Content.js
@@ -9,32 +9,26 @@ const Content = (props) => {
     <React.Fragment>
       <Container className="mt-5">
         <h1>The World Needs a Public Happiness Economy Because:</h1>
-        <h3>Short</h3>        
+        <h3>Short</h3> 
       </Container>
       {i18n.Whitepaper.Why.faq.map((item, index) => {
         return (
-           <div>
           <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
             <h5>{item.heading} </h5>
             <li className="mb-3 text-left">{item.text}</li>
             <DCSBalloon title=" " subtitle="discuss" balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
           </Col>
-          </div>
         );
-      })}
 
-      {i18n.Whitepaper.Why.faq.map((item, index) => {
         return (
-          <div>
-          <h3>Longer</h3>
-          <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
+        	<Col key={index2} className="ml-5 pl-5 mt-5" xs={11}>
+            <h3>Longer</h3>
             <p>{item.heading2} </p>
             <li className="mb-3 text-left">{item.text2}</li>
             <DCSBalloon title=" " subtitle="discuss" balloonId="bal2" display="inline" dcsTags={props.dcsTags} />
           </Col>
         );
       })}
-    </div>
     </React.Fragment>
   );
 };
