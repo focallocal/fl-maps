bb85c3a2e9494614e37b5d8fa7bd076ce0dad12f
AndyatFocallocal <focallocal.london@gmail.com>
2019-06-23 18:04:26 +0200

    trying to fix error again

--- imports/client/ui/pages/WhitePaper/Why/Content.js
+++ imports/client/ui/pages/WhitePaper/Why/Content.js
@@ -7,24 +7,26 @@ import DCSBalloon from '/imports/client/ui/components/DCSBalloon/index.js'
 const Content = (props) => {
   return (
     <React.Fragment>
-    <div>
       <Container className="mt-5">
         <h1>The World Needs a Public Happiness Economy Because:</h1>
         <h3>Short</h3>        
       </Container>
       {i18n.Whitepaper.Why.faq.map((item, index) => {
         return (
+           <div>
           <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
             <h5>{item.heading} </h5>
             <li className="mb-3 text-left">{item.text}</li>
             <DCSBalloon title=" " subtitle="discuss" balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
           </Col>
+          </div>
         );
       })}
+
       {i18n.Whitepaper.Why.faq.map((item, index) => {
         return (
+          <div>
           <h3>Longer</h3>
-          
           <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
             <p>{item.heading2} </p>
             <li className="mb-3 text-left">{item.text2}</li>
