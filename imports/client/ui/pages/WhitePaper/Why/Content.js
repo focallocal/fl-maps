e7615962039011c3101924b8ed9a3a79e512edd8
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-06-29 07:46:00 +0100

    Fixed whitepaper why bullet point rendering and mobile layout

--- imports/client/ui/pages/WhitePaper/Why/Content.js
+++ imports/client/ui/pages/WhitePaper/Why/Content.js
@@ -9,26 +9,31 @@ const Content = (props) => {
     <React.Fragment>
       <Container className="mt-5">
         <h1>The World Needs a Public Happiness Economy Because:</h1>
-        <h3>Short</h3> 
+        <br/>
+        <h3>Short</h3>
       </Container>
-      {i18n.Whitepaper.Why.faq.map((item, index) => {
+      {i18n.Whitepaper.Why.short.map((item, index) => {
         return (
-          <Col key={index} className="ml-5 pl-5 mt-5" xs={11}>
+          <Col key={index} className="pl-5 mt-5 wp-item" xs={11}>
             <h5>{item.heading} </h5>
             <li className="mb-3 text-left">{item.text}</li>
             <DCSBalloon title=" " subtitle="discuss" balloonId="bal1" display="inline" dcsTags={props.dcsTags} />
           </Col>
         );
-
+      })}
+      <Container className="mt-5">
+        <h3>Longer</h3>
+      </Container>  
+      {i18n.Whitepaper.Why.longer.map((item, index) => {
         return (
-        	<Col key={index2} className="ml-5 pl-5 mt-5" xs={11}>
-            <h3>Longer</h3>
-            <p>{item.heading2} </p>
-            <li className="mb-3 text-left">{item.text2}</li>
+          <Col key={index} className="pl-5 mt-5 wp-item" xs={11}>
+            <h5>{item.heading} </h5>
+            <li className="mb-3 text-left">{item.text}</li>
             <DCSBalloon title=" " subtitle="discuss" balloonId="bal2" display="inline" dcsTags={props.dcsTags} />
           </Col>
         );
       })}
+
     </React.Fragment>
   );
 };
