1bf5c286367ad383b744bc6db4aa569bbfc2cb41
James Sanderson <zofrex@gmail.com>
2020-04-05 15:31:22 +0100

    Add deployment settings for publichappinessmovement

---
+++ deployment/publichappinessmovement/mup.js
@@ -0,0 +1,29 @@
+var secret = require('./mup-secrets.json');
+
+module.exports = {
+  servers: {
+    one: {
+      host: '167.71.2.39',
+      username: 'deploy',
+      password: secret.password,
+    }
+  },
+  app: {
+    name: 'publichappinessmovement',
+    path: './',
+    docker: {
+      image: 'abernix/meteord:node-8.4.0-base'
+    },
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true
+    },
+    env: {
+      ROOT_URL: 'https://fl-maps.publichappinessmovement.com',
+      MONGO_URL: secret.mongo_url,
+      PORT: 8095,
+    }
+  },
+};
