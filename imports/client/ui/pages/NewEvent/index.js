ba0db08b3c9d9468aafaad4d03c4391cedb81ec2
Zahra <30802716+Zahra-8d@users.noreply.github.com>
2020-04-11 16:36:46 +0100

    Update index.js

--- imports/client/ui/pages/NewEvent/index.js
+++ imports/client/ui/pages/NewEvent/index.js
@@ -9,6 +9,7 @@ import i18n from '/imports/both/i18n/en'
 import qs from 'query-string'
 import cloneDeep from 'clone-deep'
 import './styles.scss'
+import { dcs } from "/imports/client/utils/dcs-master"
 
 const { NewEventModal: i18n_ } = i18n // Strings from i18n
 
@@ -170,6 +171,11 @@ class NewEventModal extends Component {
         this.setState({ currentStep: 0 }) // return to first step
         window.__recentEvent = { ...model, _id: res }
         this.props.history.push('/thank-you')
+        dcs.postCreateDcsTags({
+          pageName: 'lastev',
+          triggerIds: ['photos', 'videos', 'stories'],
+          notificationLevel: 3
+        })
       }
 
       window.NProgress.done()
