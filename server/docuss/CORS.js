da48baa386b1f1cb92b4cfe1efbe302f11ade093
AndyatFocallocal <focallocal.london@gmail.com>
2021-07-06 13:51:52 +0100

    Adding Syl's suggested change

--- server/docuss/CORS.js
+++ server/docuss/CORS.js
@@ -6,7 +6,8 @@ import { WebApp } from 'meteor/webapp'
 // Enable CORS on any page
 // WE SHOULD LIMIT THIS TO public/*.json
 // https://enable-cors.org/server_meteor.html
-WebApp.rawConnectHandlers.use('/', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  next()
-})
+WebApp.rawConnectHandlers.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type");
+  return next();
+});
