bad61bcba3ae5d1b1fc3d02c06c9b046e026e977
AndyatFocallocal <focallocal.london@gmail.com>
2021-07-14 11:30:51 +0100

    resolving conflict between files

--- server/docuss/CORS.js
+++ server/docuss/CORS.js
@@ -7,6 +7,7 @@ import { WebApp } from 'meteor/webapp'
 // WE SHOULD LIMIT THIS TO public/*.json
 // https://enable-cors.org/server_meteor.html
 WebApp.rawConnectHandlers.use(function(req, res, next) {
+ console.log('WebApp.rawConnectHandlers');
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type");
   return next();
