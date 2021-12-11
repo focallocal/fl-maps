f47d987ff63b2f345b3ed3b789b812c587ea33f9
AndyatFocallocal <focallocal.london@gmail.com>
2021-12-11 11:35:07 +0000

    Added Template page to repo (to be checked)

---
+++ imports/client/ui/pages/Template/FirstSection/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import i18n from '/imports/both/i18n/en'
+import DCSLink from '/imports/client/ui/components/DCSLink/index.js'
+import './styles.scss'
+
+const { title, content } = i18n.Template.first_section
+const {
+  first,
+  second,
+  third,
+  fourth,
+  fifth,
+  youtubeLink
+} = content
+
+const FirstSection = () => (
+  <section className='first-section'>
+    <Container>
+      <Row>
+        <Col xs={6}>
+          <h2 className='title'>
+            <DCSLink badge="true" format="speech-bubble" title={title} triggerId="section1" />
+          </h2>
+          <div className="text-content">
+            <p>{second}</p>
+            <p>{third}</p>
+            <p>{fourth}</p>
+            <p>{fifth}</p>
+          </div>
+        </Col>
+        <Col xs={6} className='attachment-content'>
+          <iframe src={youtubeLink} frameBorder="0" allowFullScreen></iframe>
+          <p>{first}</p>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+)
+
+export default FirstSection
---
+++ imports/client/ui/pages/Template/TopImageSection/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import i18n from '/imports/both/i18n/en'
+import './styles.scss'
+
+const { content } = i18n.Template.first_section
+const {top_image_url} = content
+
+const TopImageSection = () => (
+  <section id='top-image-section'>
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <img className='top_image' src={top_image_url}/>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+)
+
+export default TopImageSection
