136c3ba3eb59e2ccc58f1a33a3274b602f0526de
AndyatFocallocal <focallocal.london@gmail.com>
2023-10-19 03:41:15 +0300

    change to load different hero image on mobile

--- imports/client/ui/pages/Home/index.js
+++ imports/client/ui/pages/Home/index.js
@@ -1,45 +1,51 @@
-import React, { Component } from 'react'
-import FirstSection from './FirstSection'
-import SecondSection from './SecondSection'
-import HowToHelpSection from './HowToHelpSection'
-import MovementSection from './MovementSection'
-import ProjectsSection from './ProjectsSection'
-import EventsSection from './EventsSection'
-import './styles.scss'
+import React, { Component } from 'react';
+import FirstSection from './FirstSection';
+import SecondSection from './SecondSection';
+import HowToHelpSection from './HowToHelpSection';
+import MovementSection from './MovementSection';
+import ProjectsSection from './ProjectsSection';
+import EventsSection from './EventsSection';
+import './styles.scss';
 
 class Home extends Component {
-  componentDidMount () {
-    window.__setDocumentTitle('Home')
+  componentDidMount() {
+    window.__setDocumentTitle('Home');
   }
 
-  render () {
-    let url
-    let opacity
+  render() {
+    let url;
+    let opacity;
+    let isMobile = window.innerWidth <= 768; // Define the threshold for mobile devices
+
     if (window.__mapType === 'gatherings') {
-      url = '/images/PHM-logo-banner-text-mid.svg'
-      // opacity = 0.5
+      if (isMobile) {
+        url = '/images/PHM-logo-banner-low.jpg';
+      } else {
+        url = '/images/PHM-logo-banner-text-high.svg';
+      }
     } else {
-      url = '/images/focallocal-bgOG.jpg'
+      url = '/images/focallocal-bgOG.jpg';
     }
-    let backgroundImage = { backgroundImage: 'url(' + url + ')', opacity }
-    let imgStyle = { width: '100%' }
+
+    let backgroundImage = { backgroundImage: 'url(' + url + ')', opacity };
+    let imgStyle = { width: '100%' };
+
     return (
       <main className='home'>
         {/* <div id='hero-bg' style={backgroundImage} /> */}
         <div>
-          <img src={url} style={imgStyle} />
+          <img src={url} style={imgStyle} alt="Banner" />
         </div>
         <MovementSection />
         <ProjectsSection />
         <EventsSection />
-        {/*
-          <FirstSection />
-          <HowToHelpSection />
-          <SecondSection button/>
-        */}
+        {/* Uncomment the following sections if needed */}
+        {/* <FirstSection /> */}
+        {/* <HowToHelpSection /> */}
+        {/* <SecondSection button/> */}
       </main>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
