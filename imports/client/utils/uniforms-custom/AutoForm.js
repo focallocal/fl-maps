745328afa6fb9f00d480751ebc02c40224c32b67
Public Happiness Andy <focallocal.london@gmail.com>
2025-02-16 18:42:00 +0000

    another change to fix uniforms

--- imports/client/utils/uniforms-custom/AutoForm.js
+++ imports/client/utils/uniforms-custom/AutoForm.js
@@ -1,10 +1,9 @@
-import * as Uniforms from 'uniforms'
-import { QuickForm } from 'uniforms'
+import { AutoForm } from 'uniforms'
 import ValidatedQuickForm from './ValidatedQuickForm'
 // import BaseForm from 'uniforms/BaseForm' removed as AI said it wasn't used anywhere in project
 
 
-const Auto = parent => class extends Uniforms.QuickForm.Auto(parent) {
+const Auto = parent => class extends AutoForm.Auto(parent) {
     static Auto = Auto;
     onChange (key, value) {
       // starting date should not be later than ending date
