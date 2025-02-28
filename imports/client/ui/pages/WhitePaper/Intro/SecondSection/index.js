f9d116d36b902dd1bf4060c0ab08912f67def834
Public Happiness Andy <focallocal.london@gmail.com>
2025-02-28 03:33:36 +0000

    Enhance WhitePaper Intro sections with responsive video and styling
    - Updated FirstSection, SecondSection, and ThirdSection styles
    - Added aspect-ratio and responsive iframe sizing
    - Improved text content layout and spacing
    - Integrated YouTube video in SecondSection
    - Adjusted background and text alignment

--- imports/client/ui/pages/WhitePaper/Intro/SecondSection/index.js
+++ imports/client/ui/pages/WhitePaper/Intro/SecondSection/index.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import i18n from '/imports/both/i18n/en/'
-import '././style.scss'
+import './style.scss'
 
 const { title, content } = i18n.Whitepaper.Intro.second_section
 const {
   first,
   second,
-  third
+  third,
+  fourth,
+  youtubeLink
 } = content
 
 const SecondSection = () => (
   <section className='second-section'>
     <Container>
       <Row>
-        <Col md={12}>
+        <Col md={6} className='attachment-content'>
+          {youtubeLink && <iframe src={youtubeLink} frameBorder="0" allowFullScreen></iframe>}
+        </Col>
+        <Col md={6}>
           <h2 className='title'>{title}</h2>
           <div className="text-content">
             <p>{first}</p>
             <p>{second}</p>
             <p>{third}</p>
+            {fourth && <p>{fourth}</p>}
           </div>
         </Col>
       </Row>
