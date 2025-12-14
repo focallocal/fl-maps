4331b2e15de798b50de09022801c4b6ba74d1f3b
Public Happiness Andy <focallocal.london@gmail.com>
2025-12-14 22:30:07 +0000

    Fix location button: add faLocationArrow to library, set explicit 38px size

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ import '/imports/client/stylesheets/main.scss'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserCircle, faCaretDown, faBars, faHome, faCircleInfo, 
-  faShoppingCart, faWalking, faSun, faStar, faTimes} from '@fortawesome/free-solid-svg-icons'
+  faShoppingCart, faWalking, faSun, faStar, faTimes, faLocationArrow} from '@fortawesome/free-solid-svg-icons'
 
 Meteor.startup(() => {
   ensureSettingsFile()
@@ -18,7 +18,7 @@ Meteor.startup(() => {
   Meteor.subscribe('users.user') // subscribe to updated publication with custom fields
 
   library.add(faUserCircle, faCaretDown, faBars, faHome, faCircleInfo, 
-    faShoppingCart, faWalking, faSun, faStar, faTimes)
+    faShoppingCart, faWalking, faSun, faStar, faTimes, faLocationArrow)
 
   // <div id='root' style='opacity: 0;'></div>
   const rootDiv = document.createElement('div')
