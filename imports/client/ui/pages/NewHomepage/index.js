86e7658de9e1784c9f3b053d8a6a21ed64842e7b
Public Happiness Andy <focallocal.london@gmail.com>
2025-12-21 22:52:57 +0000

    Fix: Remove is-mobile class breaking desktop, rely on CSS media queries only

--- imports/client/ui/pages/NewHomepage/index.js
+++ imports/client/ui/pages/NewHomepage/index.js
@@ -55,7 +55,7 @@ class NewHomepage extends Component {
       : (backgroundConfig.desktop || DEFAULT_BACKGROUND.desktop);
 
     return (
-      <main className={`new-homepage ${isMobile ? 'is-mobile' : ''}`}>
+      <main className='new-homepage'>
         {/* Section 1: Hero Banner (unchanged) */}
         <div className='hero-section'>
           <img src={url} alt="Banner" className='background-image' />
